222880f787f3c1920573e0e339b20ca6d3702243
Nils Wisiol <info@nils-wisiol.de>
2017-04-08 09:24:46 +0200

    Setting instance and model seed separately

--- sim_learn.py
+++ sim_learn.py
@@ -3,10 +3,10 @@ from pypuf import simulation, learner, tools
 import time
 from sys import argv, stdout, stderr
 
-if len(argv) != 8:
+if len(argv) != 9:
     stderr.write('LTF Array Simulator and Logistic Regression Learner\n')
     stderr.write('Usage:\n')
-    stderr.write('sim_learn.py n k transformation combiner N restarts random_seed\n')
+    stderr.write('sim_learn.py n k transformation combiner N restarts seed_ltf seed_model\n')
     stderr.write('               n: number of bits per Arbiter chain\n')
     stderr.write('               k: number of Arbiter chains\n')
     stderr.write('  transformation: used to transform input before it is used in LTFs\n')
@@ -22,8 +22,9 @@ if len(argv) != 8:
     stderr.write('                  currently available:\n')
     stderr.write('                  - xor -- output the parity of all output bits\n')
     stderr.write('               N: number of challenge response pairs in the training set\n')
-    stderr.write('        restarts: number of repeated initializations of simulation and learner\n')
-    stderr.write('     random_seed: random seed used for reproducible results given in hex digits\n')
+    stderr.write('        restarts: number of repeated initializations the learner\n')
+    stderr.write('        seed_ltf: random seed used for LTF array instance\n')
+    stderr.write('      seed_model: random seed used for the model in first learning attempt\n')
     quit(1)
 
 n = int(argv[1])
@@ -32,9 +33,10 @@ transformation_name = argv[3]
 combiner_name = argv[4]
 N = int(argv[5])
 restarts = int(argv[6])
-random_seed = int(argv[7], 16)
+seed_ltf = int(argv[7], 16)
+seed_model = int(argv[8], 16)
 
-random.seed(random_seed) # reproduce 'random' numbers
+random.seed(seed_ltf) # reproduce 'random' numbers
 
 transformation = None
 combiner = None
@@ -53,9 +55,10 @@ except AttributeError:
 
 stderr.write('Learning %s-bit %s XOR Arbiter PUF with %s CRPs and %s restarts.\n\n' % (n, k, N, restarts))
 stderr.write('Using\n')
-stderr.write('  transformation: %s\n' % transformation)
-stderr.write('  combiner:       %s\n' % combiner)
-stderr.write('  random seed:    0x%x\n' % random_seed)
+stderr.write('  transformation:    %s\n' % transformation)
+stderr.write('  combiner:          %s\n' % combiner)
+stderr.write('  ltf random seed:   0x%x\n' % seed_ltf)
+stderr.write('  model random seed: 0x%x\n' % seed_model)
 stderr.write('\n')
 
 instance = simulation.LTFArray(
@@ -76,6 +79,8 @@ accuracy = array([])
 training_times = array([])
 iterations = array([])
 
+random.seed(seed_model)
+
 for i in range(restarts):
     stderr.write('\r%5i/%5i         ' % (i+1, restarts))
     start = time.time()
@@ -86,10 +91,11 @@ for i in range(restarts):
     accuracy = append(accuracy, 1 - dist)
     iterations = append(iterations, lr_learner.iteration_count)
     # output test result in machine-friendly format
-    # seed idx_restart n k N transformation combiner iteration_count time accuracy
+    # seed_ltf seed_model idx_restart n k N transformation combiner iteration_count time accuracy
     stdout.write(' '.join(
         [
-            '0x%x' % random_seed,
+            '0x%x' % seed_ltf,
+            '0x%x' % seed_model,
             '%5d' % i,
             '%3d' % n,
             '%2d' % k,
