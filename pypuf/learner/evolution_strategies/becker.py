4be2fdaa0533e1f7b52efb4074ea45f79ab4d1bb
chrism <chrism@mi.fu-berlin.de>
2017-06-28 01:53:13 +0200

    extend becker

--- pypuf/learner/evolution_strategies/becker.py
+++ pypuf/learner/evolution_strategies/becker.py
@@ -1,5 +1,5 @@
 import numpy as np
-from pypuf import simulation
+from pypuf.simulation.arbiter_based.ltfarray import LTFArray
 
 class reliability_based_CMA_ES():
 
@@ -37,7 +37,7 @@ class reliability_based_CMA_ES():
         terminate = False
         while not terminate:
             self.individuals = self.reproduce(self.mean, self.cov_matrix,
-                        self.parent_size, self.pop_size, self.step_size)
+                        self.pop_size, self.step_size)
             self.mean = self.update_mean()
             self.path_cm = self.cumulation_for_cm()
             self.path_ss = self.cumulation_for_ss()
@@ -47,37 +47,102 @@ class reliability_based_CMA_ES():
 
     # primary methods
     @staticmethod
-    def reproduce(mean, cov_matrix, parent_size, pop_size, step_size):
-        mutations = np.random.multivariate_normal(mean,
-                                                  cov_matrix, parent_size).T
-        mean_as_matrix = np.tile(np.matrix(mean).T, (pop_size, 1))
-        return mean_as_matrix + step_size * mutations
+    def reproduce(mean, cov_matrix, pop_size, step_size):
+        # returns a new generation of individuals as 2D array (pop_size, n)
+        mutations = np.random.multivariate_normal(np.zeros(np.shape(mean)),
+                                                  cov_matrix, pop_size).T
+        duplicated_mean = np.tile(np.matrix(mean).T, (1, pop_size))
+        return duplicated_mean + (step_size * mutations)
 
     @staticmethod
-    def update_mean():
+    def update_mean(old_mean, step_size, parent):
+        # returns mean of a new population as array (n)
+        return old_mean + step_size*parent
+
+    @staticmethod
+    def cumulation_for_cm(path_cm, c_c, path_ss, n, mu_w, parent):
+        # returns cumulated evolution path of covariance matrix
+        path_cm *= (1-c_c)
+        if(np.linalg.norm(path_ss) < 1.5 * np.sqrt(n)):
+            path_cm += np.sqrt(1 - (1-c_c)**2) * np.sqrt(mu_w) * parent
+        return path_cm
+
+    @staticmethod
+    def cumulation_for_ss(path_ss, c_sigma, mu_w, parent):
+        # returns cumulated evolution path of step-size
+        path_ss = (1-c_sigma) * path_ss + np.sqrt(1 - (1-c_sigma)**2)\
+                                          * np.sqrt(mu_w) * parent
+        return path_ss
+
+    @staticmethod
+    def update_cm(cov_matrix, c_1, c_mu, path_cm, w_i, y_i):
+        # returns covariance matrix of a new population (pop_size, pop_size)
         pass
 
     @staticmethod
-    def cumulation_for_cm():
-        pass
-
-    @staticmethod
-    def cumulation_for_ss():
-        pass
-
-    @staticmethod
-    def update_cm():
-        pass
-
-    @staticmethod
-    def update_ss():
+    def update_ss(step_size, c_sigma, d_sigma, path_ss):
+        # returns step-size of a new population
         pass
 
 
     # secondary methods
-    def fitness(self):
+    @staticmethod
+    def get_parent(sorted_individuals, parent_size, priorities):
+        # returns the weighted sum of the fittest individuals
+        parent = np.empty(np.shape(sorted_individuals)[1])
+        for i in range(parent_size):
+            parent += priorities[i] * sorted_individuals[i,:]
+        return parent
+
+    @staticmethod
+    def fitness(instance, challenges, reliabilities, individuals):
+        """
+        build ltfarrays out of individuals
+        get delay differences from ltfarrays for challenges
+        get reliability vector out of delay differences
+        get correlation coefficient of reliability vectors
+        sort individuals by correlation coefficients
+        return sorted individuals
+        """
+        # returns individuals sorted by their fitness
         pass
 
-    def reliability(self, l):
-        #l/2 - r[i][j]
+
+    def build_ltf_arrays(individuals):
+        # returns iterator over ltf_arrays created out of every individual
+        transform = LTFArray.transform_id
+        combiner = LTFArray.combiner_xor
+        pop_size = np.shape(individuals)[1]
+        for i in range(pop_size):
+            yield LTFArray(individuals.T[:, i], transform, combiner,
+                           bias=False)
+
+    def get_delay_differences(ltf_arrays, pop_size, challenges, challenge_num):
+        # returns 2D array of delay differences for all challenges on every
+        #   individual
+        delay_diffs = np.empty(pop_size, challenge_num)
+        for i, ltf_array in ltf_arrays:
+            for j, challenge in challenges:
+                delay_diffs[i][j] = ltf_array.val(challenge)
+        return delay_diffs
+
+    def get_reliabilities(delay_diffs, epsilon):
+        # returns 2D array of reliabilities for all challenges on every individual
+        reliabilities = np.zeros(np.shape(delay_diffs))
+        indices_of_unreliable = delay_diffs > -epsilon and delay_diffs < epsilon
+        reliabilities[indices_of_unreliable] = 1
+        return reliabilities
+
+    def get_correlations(reliabilities, measured_rels):
+        # returns array of pearson correlation coefficients between reliability
+        #   array of individual and instance for all individuals
+        pop_size = np.shape(reliabilities[0])
+        correlations = np.empty(pop_size)
+        for i in range(pop_size):
+            correlations[i] = np.corrcoef(reliabilities[i,:], measured_rels)[0, 1]
+        return correlations
+
+    def sort_individuals(individuals, correlations):
+        # returns 2D array of individuals as given from input, but sorted through
+        #   correlation coefficients
         pass
--- pypuf/tools.py
+++ pypuf/tools.py
@@ -1,5 +1,5 @@
 from numpy import random, count_nonzero, array, append
-import itertools
+import itertools as it
 
 
 def random_input(n):
@@ -9,29 +9,34 @@ def random_input(n):
     return random.choice((-1, +1), n)
 
 
-def all_inputs(n):
+def all_inputs(n, repeat=1):
     """
-    returns an iterator for all {-1,1}-vectors of length `n`.
+    returns an iterator for all {-1,1}-vectors of length `n`, each repeatedly.
     """
-    return itertools.product((-1, +1), repeat=n)
+    for input in it.product((-1, +1), repeat=n):
+        for repeated in it.repeat(input, times=repeat):
+            yield array(repeated)
 
 
-def random_inputs(n, num):
+def random_inputs(n, num, repeat=1):
     """
-    returns an iterator for a random sample of {-1,1}-vectors of length `n` (with replacement).
+    returns an iterator for a random sample of {-1,1}-vectors of length `n`
+    (with replacement), each repeatedly.
     """
     for i in range(num):
-        yield random_input(n)
+        for input in it.repeat(random_input(n), times=repeat):
+            yield input
 
 
-def sample_inputs(n, num):
+def sample_inputs(n, num, repeat=1):
     """
-    returns an iterator for either random samples of {-1,1}-vectors of length `n` if `num` < 2^n,
-    and an iterator for all {-1,1}-vectors of length `n` otherwise.
+    Returns an iterator for either random samples or all possible samples of
+    {-1,1}-vectors of length `n`, each repeatedly, if required.
     Note that we return only 2^n vectors even with `num` > 2^n.
-    In other words, the output of this function is deterministic if and only if num >= 2^n.
+    I.e. the output of this function is deterministic iff num >= 2^n.
     """
-    return random_inputs(n, num) if num < 2**n else all_inputs(n)
+    return random_inputs(n, num, repeat) if num < 2**n else all_inputs(n, repeat)
+
 
 def iter_append_last(array_iterator, x):
     """
@@ -39,6 +44,7 @@ def iter_append_last(array_iterator, x):
     """
     for array in array_iterator:
         yield append(array, x)
+
 
 def approx_dist(a, b, num):
     """
@@ -50,6 +56,7 @@ def approx_dist(a, b, num):
     d = 0
     inputs = array(list(random_inputs(a.n, num)))
     return (num - count_nonzero(a.eval(inputs) == b.eval(inputs))) / num
+
 
 def compare_functions(x, y):
     """
@@ -64,6 +71,7 @@ def compare_functions(x, y):
     # The bytcode maybe differ from each other https://stackoverflow.com/a/20059029
     b &= xc.co_name == yc.co_name
     return b and xc.co_filename == yc.co_filename
+
 
 class TrainingSet():
     """
