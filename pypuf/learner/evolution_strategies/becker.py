bb2af90bad570dcf6c02683d77c98b7f97f3002b
chrism <chrism@mi.fu-berlin.de>
2017-07-02 19:26:15 +0200

    update becker

--- pypuf/learner/evolution_strategies/becker.py
+++ pypuf/learner/evolution_strategies/becker.py
@@ -1,28 +1,29 @@
 import numpy as np
+import scipy as sp
+from pypuf import tools
 from pypuf.simulation.arbiter_based.ltfarray import LTFArray
 
-class reliability_based_CMA_ES():
+class Reliability_based_CMA_ES():
 
     # recommended properties of parameters:
     #   pop_size=30
     #   parent_size=10 (>= 0.3*pop_size)
     #   priorities: linear low decreasing
-    def __init__(self, instance, pop_size, parent_size, priorities,
-                 mu_weight=0, sigma_weight=1):
-        self.instance = instance            # simulation instance to be modelled
-        self.mu_weight = mu_weight          # mean value of weights
-        self.sigma_weight = sigma_weight    # sd of weights
+    def __init__(self, instance, pop_size, parent_size, priorities, challenge_num, repeat):
+        self.instance = instance    # simulation instance to be modelled
+        self.challenge_num = challenge_num    # number of challenges used
+        self.repeat = repeat        # frequency of same repeated challenge
         self.individuals = np.zeros((instance.n, parent_size))
         # mean, step_size,  pop_size,   parent_size,    priorities, cov_matrix,   path_cm,    path_ss
         # m,    sigma,      lambda,     mu,             w_i,        C,            p_c,        p_sigma
         self.mean = np.zeros(instance.n) # mean vector of distribution
-        self.step_size = sigma_weight / 2   # distance to next distribution
+        self.step_size = 1              # distance to next distribution
         self.pop_size = pop_size        # number of individuals per generation
         self.parent_size = parent_size  # number of considered individuals
         self.priorities = priorities    # array of consideration proportion
         self.cov_matrix = np.identity(instance.n)  # shape of distribution
-        self.path_cm = 0    # cumulated evolution path of covariance matrix
-        self.path_ss = 0    # cumulated evolution path of step size
+        self.path_cm = np.zeros(instance.n) # cumulated evolution path of covariance matrix
+        self.path_ss = np.zeros(instance.n) # cumulated evolution path of step size
         # auxiliary constants
         self.mu_w = 1 / np.sum(np.square(priorities))
         self.c_mu = self.mu_w / instance.n**2
@@ -30,64 +31,111 @@ class reliability_based_CMA_ES():
         self.c_1 = 2 / instance.n**2
         self.c_c = 4 / instance.n
         self.c_sigma = 4 / instance.n
-        assert len(priorities)==parent_size and np.sum(priorities)==1
-        assert (self.c_1 + self.mu_w <= 1)
+        assert len(priorities)==parent_size
+        assert int(np.ndarray.sum(priorities))==1
+        assert (self.c_1 + self.c_mu <= 1)
 
     def learn(self):
+        epsilon = self.learn_epsilon(self.instance)
         terminate = False
         while not terminate:
             self.individuals = self.reproduce(self.mean, self.cov_matrix,
-                        self.pop_size, self.step_size)
-            self.mean = self.update_mean()
-            self.path_cm = self.cumulation_for_cm()
-            self.path_ss = self.cumulation_for_ss()
-            self.cov_matrix = self.update_cm()
-            self.step_size = self.update_ss()
+                                              self.pop_size, self.step_size)
+            challenges = tools.sample_inputs(self.instance.n, self.challenge_num)
+            measured_rels = self.measure_rels(self.instance, challenges,
+                                              self.challenge_num, self.repeat, epsilon)
+            sorted_individuals = self.fitness(challenges, self.challenge_num,
+                                              measured_rels, self.individuals, epsilon)
+            parent = self.get_parent(sorted_individuals, self.parent_size,
+                                     self.priorities)
+            self.mean = self.update_mean(self.mean, self.step_size, parent)
+            self.path_cm = self.cumulation_for_cm(self.path_cm, self.c_c,
+                                                  self.path_ss, self.instance.n, self.mu_w, parent)
+            self.path_ss = self.cumulation_for_ss(self.path_ss, self.c_sigma,
+                                                  self.mu_w, self.cov_matrix, parent)
+            cm_mu = self.get_cm_mu(sorted_individuals, self.parent_size,
+                                   self.priorities)
+            self.cov_matrix = self.update_cm(self.cov_matrix, self.c_1,
+                                             self.c_mu, self.path_cm, cm_mu)
+            self.step_size = self.update_ss(self.step_size, self.c_sigma,
+                                            self.d_sigma, self.path_ss)
 
-
-    # primary methods
+    # updating methods of evolution strategies
     @staticmethod
     def reproduce(mean, cov_matrix, pop_size, step_size):
         # returns a new generation of individuals as 2D array (pop_size, n)
         mutations = np.random.multivariate_normal(np.zeros(np.shape(mean)),
-                                                  cov_matrix, pop_size).T
-        duplicated_mean = np.tile(np.matrix(mean).T, (1, pop_size))
-        return duplicated_mean + (step_size * mutations)
+                                                  cov_matrix, pop_size)
+        duplicated_mean = np.tile(np.matrix(mean), (pop_size, 1))
+        return (duplicated_mean + (step_size * mutations))
 
     @staticmethod
-    def update_mean(old_mean, step_size, parent):
+    def update_mean(mean, step_size, parent):
         # returns mean of a new population as array (n)
-        return old_mean + step_size*parent
+        return mean + step_size*parent
 
     @staticmethod
     def cumulation_for_cm(path_cm, c_c, path_ss, n, mu_w, parent):
         # returns cumulated evolution path of covariance matrix
-        path_cm *= (1-c_c)
+        path_cm = path_cm * (1-c_c)
         if(np.linalg.norm(path_ss) < 1.5 * np.sqrt(n)):
-            path_cm += np.sqrt(1 - (1-c_c)**2) * np.sqrt(mu_w) * parent
+            path_cm = path_cm + (np.sqrt(1 - (1-c_c)**2) *
+                                 np.sqrt(mu_w) * parent)
         return path_cm
 
     @staticmethod
     def cumulation_for_ss(path_ss, c_sigma, mu_w, cov_matrix, parent):
+        # TODO
         # returns cumulated evolution path of step-size
-        return (1 - c_sigma) * path_ss + np.sqrt(1 - (1 - c_sigma)**2) \
-                * np.sqrt(mu_w) * cov_matrix**(1/2) * parent
+        return (1-c_sigma) * path_ss + np.sqrt(1 - (1-c_sigma)**2) * \
+                            np.sqrt(mu_w) * cov_matrix^(-1/2) * parent
 
     @staticmethod
-    def update_cm(cov_matrix, c_1, c_mu, path_cm, parent_product):
+    def update_cm(cov_matrix, c_1, c_mu, path_cm, cm_mu):
         # returns covariance matrix of a new population (pop_size, pop_size)
-        return (1 - c_1 - c_mu) * cov_matrix + \
-                     c_1 * path_cm * path_cm.T + c_mu * parent_product
+        return (1 - c_1 - c_mu) * cov_matrix + c_1 * path_cm * path_cm.T \
+                + c_mu * cm_mu
 
     @staticmethod
     def update_ss(step_size, c_sigma, d_sigma, path_ss):
         # returns step-size of a new population
         factor = np.exp((c_sigma / d_sigma) *
-                ((np.linalg.norm(path_ss) / np.zeros(np.shape(path_ss))) - 1))
+                ((np.linalg.norm(path_ss) / np.sqrt(np.shape(path_ss)[1])) - 1))
         return step_size * factor
 
 
     # secondary methods
+    @staticmethod
+    def learn_epsilon(instance):
+        # returns epsilon on the assumption: 2% of challenges are unreliable
+        n = instance.n
+        num = 1000
+        repeat = 100
+        challenges = tools.sample_inputs(n, num)
+        ratios = np.zeros(num)
+        responses = np.zeros(num, repeat)
+        for i, challenge in enumerate(challenges):
+            for j in range(repeat):
+                responses[i, j] = instance.eval(challenge)
+            ratio = np.sum(responses[i, :]) / repeat
+            ratios[i] = ratio if ratio <= 0.5 else (1 - ratio)
+        ratios = np.sort(ratios)
+        return np.sum(ratios[15:25]) / 10
+
+    @staticmethod
+    def measure_rels(instance, challenges, challenge_num, repeat, epsilon):
+        # returns array of measured reliabilities of instance
+        measured_rels = np.zeros(challenge_num)
+        responses = np.zeros(challenge_num, repeat)
+        for i, challenge in enumerate(challenges):
+            for j in range(repeat):
+                responses[i, j] = instance.eval(challenge)
+            cumulation_ratio = np.sum(responses[i, :]) / repeat
+            if cumulation_ratio > epsilon and cumulation_ratio <= 0.5 or \
+                                    (1 - cumulation_ratio) > epsilon and (1 - cumulation_ratio) <= 0.5:
+                measured_rels[i] = 1
+        return measured_rels
+
     @staticmethod
     def get_parent(sorted_individuals, parent_size, priorities):
         # returns the weighted sum of the fittest individuals
@@ -97,19 +145,17 @@ class reliability_based_CMA_ES():
         return parent
 
     @staticmethod
-    def get_parent_product(sorted_individuals, parent_size, priorities):
+    def get_cm_mu(sorted_individuals, parent_size, priorities):
         # returns the weighted sum of the fittest individuals
-        parent_product = np.empty(np.shape(sorted_individuals)[1])
+        cm_mu = np.empty(np.shape(sorted_individuals)[1])
         for i in range(parent_size):
-            parent_product += priorities[i] * sorted_individuals[i,:] * \
+            cm_mu += priorities[i] * sorted_individuals[i,:] * \
                               sorted_individuals[i,:].T
-        return parent_product
+        return cm_mu
 
     @staticmethod
-    def fitness(instance, challenges, reliabilities, individuals):
+    def fitness(challenges, challenge_num, measured_rels, individuals, epsilon):
         """
-        Daihyun Lim. Extracting Secret Keys from Integrated
-Circuits Msc thesis, MIT, 2004.
         build ltfarrays out of individuals
         get delay differences from ltfarrays for challenges
         get reliability vector out of delay differences
@@ -118,9 +164,15 @@ Circuits Msc thesis, MIT, 2004.
         return sorted individuals
         """
         # returns individuals sorted by their fitness
-        pass
+        pop_size = np.shape(individuals)[0]
+        ltf_arrays = Reliability_based_CMA_ES.build_ltf_arrays(individuals)
+        delay_diffs = Reliability_based_CMA_ES.get_delay_differences(
+            ltf_arrays, pop_size, challenges, challenge_num)
+        reliabilities = Reliability_based_CMA_ES.get_reliabilities(delay_diffs, epsilon)
+        correlations = Reliability_based_CMA_ES.get_correlations(reliabilities, measured_rels)
+        return Reliability_based_CMA_ES.sort_individuals(individuals, correlations)
 
-
+    @staticmethod
     def build_ltf_arrays(individuals):
         # returns iterator over ltf_arrays created out of every individual
         transform = LTFArray.transform_id
@@ -130,15 +182,17 @@ Circuits Msc thesis, MIT, 2004.
             yield LTFArray(individuals.T[:, i], transform, combiner,
                            bias=False)
 
+    @staticmethod
     def get_delay_differences(ltf_arrays, pop_size, challenges, challenge_num):
         # returns 2D array of delay differences for all challenges on every
         #   individual
         delay_diffs = np.empty(pop_size, challenge_num)
-        for i, ltf_array in ltf_arrays:
+        for i, ltf_array in enumerate(ltf_arrays):
             for j, challenge in challenges:
                 delay_diffs[i][j] = ltf_array.val(challenge)
         return delay_diffs
 
+    @staticmethod
     def get_reliabilities(delay_diffs, epsilon):
         # returns 2D array of reliabilities for all challenges on every individual
         reliabilities = np.zeros(np.shape(delay_diffs))
@@ -146,6 +200,7 @@ Circuits Msc thesis, MIT, 2004.
         reliabilities[indices_of_unreliable] = 1
         return reliabilities
 
+    @staticmethod
     def get_correlations(reliabilities, measured_rels):
         # returns array of pearson correlation coefficients between reliability
         #   array of individual and instance for all individuals
@@ -155,7 +210,9 @@ Circuits Msc thesis, MIT, 2004.
             correlations[i] = np.corrcoef(reliabilities[i,:], measured_rels)[0, 1]
         return correlations
 
+    @staticmethod
     def sort_individuals(individuals, correlations):
+        # TODO
         # returns 2D array of individuals as given from input, but sorted through
         #   correlation coefficients
         pass
--- pypuf/learner/evolution_strategies/test_becker.py
+++ pypuf/learner/evolution_strategies/test_becker.py
@@ -1,10 +1,11 @@
-from pypuf.learner.evolution_strategies.becker import reliability_based_CMA_ES as becker
-from pypuf.simulation.arbiter_based.ltfarray import NoisyLTFArray
-from pypuf import tools
+import unittest
 import numpy as np
+from pypuf import tools
+from pypuf.learner.evolution_strategies.becker import reliability_based_CMA_ES as becker
+from pypuf.simulation.arbiter_based.ltfarray import LTFArray, NoisyLTFArray
 
-n = 8
-k = 4
+n = 6
+k = 2
 mu = 0
 sigma = 1
 weight_array = NoisyLTFArray.normal_weights(n, k, mu, sigma)
@@ -13,13 +14,128 @@ combiner = NoisyLTFArray.combiner_xor
 noisiness = 0.1
 sigma_noise = NoisyLTFArray.sigma_noise_from_random_weights(n, sigma, noisiness)
 
+instance = NoisyLTFArray(weight_array, transform, combiner, sigma_noise)
+#print(vars(instance))
 
-instance = NoisyLTFArray.__init__(weight_array, transform, combiner, sigma_noise, noise_seed, bias)
-pop_size = 30
-parent_size = 9
-priorities = np.array([.20, .17, .15, .13, .11, .9, .7, .5, .3])
-becker_learner = becker(instance, pop_size, parent_size, priorities,
-                 mu_weight=0, sigma_weight=1)
+pop_size = 5
+parent_size = 4
+priorities = np.array([.25, .25, .25, .25])
+
+#pop_size = 30
+#parent_size = 10
+#priorities = np.array([.15, .14, .13, .12, .11, .09, .08, .07, .06, .05])
+#priorities = np.array([.20, .15, .12, .10, .08, .07, .07, .07, .07, .07])
+bl = becker(instance, pop_size, parent_size, priorities)
+#print(vars(becker_learner))
+
+print(bl.mean, '\n', bl.cov_matrix, '\n', bl.pop_size, '\n', bl.step_size)
+
+generation = becker.reproduce(bl.mean, bl.cov_matrix, bl.pop_size, bl.step_size)
+print(generation)
+
+class TestLTFArray(unittest.TestCase):
+
+    weight_array_1 = np.array([[0.2, -0.7, 2.4, 0.6, 0.1, -0.9, -1.1, 0.5],
+                               [-2.0, 0.7, 1.2, -0.2, 1.3, -0.8, -0.4, 0.9]])
+    weight_array_2 = np.array([[1.5, 0.3, -1.2, 0.9, -0.4],
+                               [0.1, -2.1, -0.6, 1.4, -0.7]])
+    weight_array_3 = LTFArray.normal_weights(32, 4, 1, 3)
+    weight_array_4 = LTFArray.normal_weights(64, 8, 0, 1)
+
+    transform = LTFArray.transform_id
+    combiner = LTFArray.combiner_xor
+
+    inst_1 = NoisyLTFArray(weight_array_1, transform, combiner,
+                          0.1*np.sqrt(np.shape(weight_array_1)[1]), bias=False)
+    inst_2 = NoisyLTFArray(weight_array_2, transform, combiner,
+                          0.1*np.sqrt(np.shape(weight_array_2)[1]), bias=True)
+    inst_3 = NoisyLTFArray(weight_array_3, transform, combiner,
+                          0.3 * np.sqrt(np.shape(weight_array_3)[1]), bias=False)
+    inst_4 = NoisyLTFArray(weight_array_4, transform, combiner,
+                           0.1 * np.sqrt(np.shape(weight_array_4)[1]), bias=False)
+
+    prio_1 = np.array([0.4, 0.3, 0.2, 0.1])
+    prio_2 = np.array([.15, .14, .13, .12, .11, .09, .08, .07, .06, .05])
+
+    test_set = [
+        # instance, pop_size, parent_size, priorities
+        (inst_1, 8, 4, prio_1),
+        (inst_1, 30, 10, prio_2),
+        (inst_2, 8, 4, prio_1),
+        (inst_2, 30, 10, prio_2),
+        (inst_3, 8, 4, prio_1),
+        (inst_3, 30, 10, prio_2),
+        (inst_4, 8, 4, prio_1),
+        (inst_4, 30, 10, prio_2)
+    ]
+
+    instance = instance  # simulation instance to be modelled
+    individuals = np.zeros((instance.n, parent_size))
+    # mean, step_size,  pop_size,   parent_size,    priorities, cov_matrix,   path_cm,    path_ss
+    # m,    sigma,      lambda,     mu,             w_i,        C,            p_c,        p_sigma
+    mean = np.zeros(instance.n)  # mean vector of distribution
+    step_size = 1  # distance to next distribution
+    pop_size = pop_size  # number of individuals per generation
+    parent_size = parent_size  # number of considered individuals
+    priorities = priorities  # array of consideration proportion
+    cov_matrix = np.identity(instance.n)  # shape of distribution
+    path_cm = 0  # cumulated evolution path of covariance matrix
+    path_ss = 0  # cumulated evolution path of step size
+    # auxiliary constants
+    mu_w = 1 / np.sum(np.square(priorities))
+    c_mu = mu_w / instance.n ** 2
+    d_sigma = 1 + np.sqrt(mu_w / instance.n)
+    c_1 = 2 / instance.n ** 2
+    c_c = 4 / instance.n
+    c_sigma = 4 / instance.n
+
+    # primary methods
+    def test_reproduce(self):
+        for params in self.test_set:
+            shape = np.shape(params[0].weight_array)[1]
+            reproduction = becker.reproduce(mean = np.zeros(shape),
+                cov_matrix = np.identity(shape), pop_size = params[1],
+                step_size = 1)
+        matrix = np.array()
+        np.testing.assert_array_equal(reproduction, matrix)
+
+    def test_update_mean(self):
+        becker.update_mean(old_mean, step_size, parent)
+
+    def test_cumulation_for_cm(self):
+        becker.cumulation_for_cm(path_cm, c_c, path_ss, n, mu_w, parent)
+
+    def test_cumulation_for_ss(self):
+        becker.cumulation_for_ss(path_ss, c_sigma, mu_w, cov_matrix, parent)
+
+    def test_update_cm(self):
+        becker.update_cm(cov_matrix, c_1, c_mu, path_cm, parent_product)
+
+    def test_update_ss(self):
+        becker.update_ss(step_size, c_sigma, d_sigma, path_ss)
 
 
-generation = becker.reproduce(mean, cov_matrix, pop_size, step_size)
+    # secondary methods
+    def test_get_parent(self):
+        becker.get_parent(sorted_individuals, parent_size, priorities)
+
+    def test_get_parent_product(self):
+        becker.get_parent_product(sorted_individuals, parent_size, priorities)
+
+    def test_fitness(self):
+        becker.fitness(instance, challenges, reliabilities, individuals)
+
+    def test_build_ltf_arrays(self):
+        becker.build_ltf_arrays(individuals)
+
+    def test_get_delay_differences(self):
+        becker.get_delay_differences(ltf_arrays, pop_size, challenges, challenge_num)
+
+    def test_get_reliabilities(self):
+        becker.get_reliabilities(delay_diffs, epsilon)
+
+    def test_get_correlations(self):
+        becker.get_correlations(reliabilities, measured_rels)
+
+    def test_sort_individuals(self):
+        becker.sort_individuals(individuals, correlations)
