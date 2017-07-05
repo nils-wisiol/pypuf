f6c1e2dcaf197f8b1bf1dabf161521fef51458a3
MRM0nkey <yoda_1991@hotmail.de>
2017-07-05 13:41:46 +0200

    Adding test for LR experiment.

---
+++ test/test_experiment_logistic_regression.py
@@ -0,0 +1,16 @@
+import unittest
+from pypuf.simulation.arbiter_based.ltfarray import LTFArray
+from pypuf.experiments.experiment.logistic_regression import ExperimentLogisticRegression
+
+
+class TestExperimentLogisticRegression(unittest.TestCase):
+    def setUp(self):
+        self.lr16_4 = ExperimentLogisticRegression('exp1.log', 8, 2, 12000, 0xbeef, 0xbeef, LTFArray.transform_id,
+                                                   LTFArray.combiner_xor, restarts=6)
+        self.lr16_4.analysis()
+
+    def test_lr_experiment_output_string(self):
+        self.assertNotEqual(self.lr16_4.output_string(), '')
+
+    def test_lr_experiment_name(self):
+        self.assertNotEqual(self.lr16_4.name, '')
