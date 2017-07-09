be293ad73dee9af5e5fa1b7fba144c229b570e2a
chrism <chrism@mi.fu-berlin.de>
2017-07-09 17:49:33 +0200

    finish implementing all methods; needs to be tested

--- pypuf/learner/evolution_strategies/becker.py
+++ pypuf/learner/evolution_strategies/becker.py
@@ -1,4 +1,6 @@
 import numpy as np
+import scipy.stats as stats
+import itertools
 from pypuf import tools
 from pypuf.simulation.arbiter_based.ltfarray import LTFArray
 
@@ -8,13 +10,19 @@ class Reliability_based_CMA_ES():
     #   pop_size=30
     #   parent_size=10 (>= 0.3*pop_size)
     #   priorities: linear low decreasing
-    def __init__(self, instance, pop_size, parent_size, priorities, challenge_num, repeat):
-        self.different_LTFs = np.zeros(instance.k, instance.n) # learned LTFs
+    def __init__(self, instance, pop_size, parent_size, priorities, challenge_num, repeat, unreliability):
+        self.transform = LTFArray.transform_id
+        self.combiner = LTFArray.combiner_xor
+        self.different_LTFs = np.zeros((instance.k, instance.n)) # learned LTFs
+        self.unreliability = unreliability  # proportion of unreliable challenges
         self.epsilon = 0            # learned threshold for reliability
+        self.num_of_LTFs = 0        # number of different learned LTFs
+        self.new_LTF = np.empty(instance.n) # current learned LTF
+        self.terminate = False      # termination variable for learning 1 LTF
         self.instance = instance    # simulation instance to be modelled
         self.challenge_num = challenge_num    # number of challenges used
         self.repeat = repeat        # frequency of same repeated challenge
-        self.individuals = np.zeros((instance.n, parent_size))
+        self.individuals = np.zeros((parent_size, instance.n))  # weight_arrays
         # mean, step_size,  pop_size,   parent_size,    priorities, cov_matrix,   path_cm,    path_ss
         # m,    sigma,      lambda,     mu,             w_i,        C,            p_c,        p_sigma
         self.mean = np.zeros(instance.n) # mean vector of distribution
@@ -23,7 +31,7 @@ class Reliability_based_CMA_ES():
         self.parent_size = parent_size  # number of considered individuals
         self.priorities = priorities    # array of consideration proportion
         self.cov_matrix = np.identity(instance.n)  # shape of distribution
-        self.path_cm = np.zeros(instance.n) # cumulated evolution path of covariance matrix
+        self.path_cm = np.matrix(np.zeros(instance.n)) # cumulated evolution path of covariance matrix
         self.path_ss = np.zeros(instance.n) # cumulated evolution path of step size
         # auxiliary constants
         self.mu_w = 1 / np.sum(np.square(priorities))
@@ -32,41 +40,55 @@ class Reliability_based_CMA_ES():
         self.c_1 = 2 / instance.n**2
         self.c_c = 4 / instance.n
         self.c_sigma = 4 / instance.n
-        assert len(priorities)==parent_size
-        assert int(np.ndarray.sum(priorities))==1
+        assert len(priorities) == parent_size
+        assert int(np.ndarray.sum(priorities)) == 1
         assert (self.c_1 + self.c_mu <= 1)
 
     def learn(self):
-        self.epsilon = self.learn_epsilon(self.instance)
-        num_of_LTFs = 0
-        while num_of_LTFs < self.instance.k:
-            new_LTF = self.learn_LTF()
-            if self.is_different_LTF(new_LTF, num_of_LTFs):
-                self.different_LTFs[num_of_LTFs] = new_LTF
-                num_of_LTFs += 1
-        transform = LTFArray.transform_id
-        combiner = LTFArray.combiner_xor
-        return LTFArray(self.different_LTFs, transform, combiner, bias=False)
+        # this is the main learning method
+        # returns XOR-LTFArray with nearly same behavior as learned instance
+        self.epsilon = self.learn_epsilon(self.instance, self.unreliability)
+        while self.num_of_LTFs < self.instance.k:
+            self.new_LTF = self.learn_LTF()
+            if self.is_different_LTF():
+                self.different_LTFs[self.num_of_LTFs] = self.new_LTF
+                self.num_of_LTFs += 1
+        self.different_LTFs = self.set_pole_of_LTFs(self.instance, self.different_LTFs)
+        return LTFArray(self.different_LTFs, self.transform, self.combiner, bias=False)
 
-    def is_different_LTF(self, num_of_LTFs):
-        # TODO
-        reliabilities = self.get_reliabilities(self.different_LTFs, self.epsilon)
-        # ???
-        self.get_correlations(reliabilities, measured_rels)
-        pass
+    def is_different_LTF(self):
+        # returns True iff new_LTF is different from previously learned LTFs
+        if self.num_of_LTFs == 0 :
+            return True
+        weight_arrays = self.different_LTFs[:self.num_of_LTFs-1, :]
+        new_LTFArray = LTFArray(self.new_LTF, self.transform, self.combiner)
+        different_LTFs = self.build_LTFArrays(weight_arrays)
+        challenges = tools.sample_inputs(self.instance.n, self.challenge_num)
+        responses = np.empty((self.num_of_LTFs, self.challenge_num))
+        for i, challenge in enumerate(challenges):
+            responses[0, i] = new_LTFArray.eval(challenge)
+            for j, current_LTF in enumerate(different_LTFs):
+                responses[j+1, i] = current_LTF.eval(challenge)
+        return not self.is_correlated(responses)
 
     def learn_LTF(self):
-        # TODO
+        # this is the main CMA-ES algorithm like that from Hansen
         terminate = False
         while not terminate:
             challenges = tools.sample_inputs(
-                self.instance.n, self.challenge_num)
-            measured_rels = self.measure_rels(
-                self.instance, challenges, self.challenge_num, self.repeat,
-                self.epsilon)
+                    self.instance.n, self.challenge_num)
+            measured_rels = np.zeros(self.challenge_num)
+            while np.sum(measured_rels) < (1-self.unreliability)*self.challenge_num:
+                challenges = tools.sample_inputs(
+                    self.instance.n, self.challenge_num)
+                measured_rels = self.measure_rels(
+                    self.instance, challenges, self.challenge_num, self.repeat,
+                    self.epsilon)
             sorted_individuals = self.fitness(
                 challenges, self.challenge_num, measured_rels,
                 self.individuals, self.epsilon)
+            if self.terminate:
+                break
             parent = self.get_parent(
                 sorted_individuals, self.parent_size, self.priorities)
             cm_mu = self.get_cm_mu(
@@ -84,7 +106,7 @@ class Reliability_based_CMA_ES():
                 self.cov_matrix, self.c_1, self.c_mu, self.path_cm, cm_mu)
             self.step_size = self.update_ss(
                 self.step_size, self.c_sigma, self.d_sigma, self.path_ss)
-        return self.mean
+        return self.new_LTF
 
 
     # updating methods of evolution strategies
@@ -94,7 +116,7 @@ class Reliability_based_CMA_ES():
         mutations = np.random.multivariate_normal(np.zeros(np.shape(mean)),
                                                   cov_matrix, pop_size)
         duplicated_mean = np.tile(np.matrix(mean), (pop_size, 1))
-        return (duplicated_mean + (step_size * mutations))
+        return duplicated_mean + (step_size * mutations)
 
     @staticmethod
     def update_mean(mean, step_size, parent):
@@ -113,7 +135,7 @@ class Reliability_based_CMA_ES():
     @staticmethod
     def cumulation_for_ss(path_ss, c_sigma, mu_w, cov_matrix, parent):
         # returns cumulated evolution path of step-size
-        cm_eigen_dec = Reliability_based_CMA_ES.eigen_decomposition(cov_matrix)
+        cm_eigen_dec = Reliability_based_CMA_ES.modify_eigen_decomposition(cov_matrix)
         return (1-c_sigma) * path_ss + np.sqrt(1 - (1-c_sigma)**2) * \
                                        np.sqrt(mu_w) * cm_eigen_dec @ parent
 
@@ -130,49 +152,128 @@ class Reliability_based_CMA_ES():
                         ((np.linalg.norm(path_ss) / np.sqrt(np.shape(path_ss)[1])) - 1))
         return step_size * factor
 
-
-    # secondary methods
-    @staticmethod
-    def fitness(challenges, challenge_num, measured_rels, individuals, epsilon):
-        """
-        build ltfarrays out of individuals
-        get delay differences from ltfarrays for challenges
-        get reliability vector out of delay differences
-        get correlation coefficient of reliability vectors
-        sort individuals by correlation coefficients
-        return sorted individuals
-        """
+    def fitness(self, challenges, challenge_num, measured_rels, individuals, epsilon, epsilon_theo):
         # returns individuals sorted by their fitness
         pop_size = np.shape(individuals)[0]
-        ltf_arrays = Reliability_based_CMA_ES.build_ltf_arrays(individuals)
+        built_LTFs = Reliability_based_CMA_ES.build_LTFArrays(individuals)
         delay_diffs = Reliability_based_CMA_ES.get_delay_differences(
-            ltf_arrays, pop_size, challenges, challenge_num)
-        reliabilities = Reliability_based_CMA_ES.get_reliabilities(delay_diffs, epsilon)
+            built_LTFs, pop_size, challenges, challenge_num)
+        reliabilities = Reliability_based_CMA_ES.get_reliabilities(delay_diffs, epsilon_theo)
         correlations = Reliability_based_CMA_ES.get_correlations(reliabilities, measured_rels)
+        for i in range(np.shape(correlations)):
+            if correlations[i] > 0.9*epsilon:
+                self.new_LTF = individuals[i, :]
+                self.terminate = True
         return Reliability_based_CMA_ES.sort_individuals(individuals, correlations)
 
+    # methods for calculating fitness
     @staticmethod
-    def learn_epsilon(instance):
-        # returns epsilon on the assumption: 2% of challenges are unreliable
+    def build_LTFArrays(individuals):
+        # returns iterator over ltf_arrays created out of every individual
+        transform = LTFArray.transform_id
+        combiner = LTFArray.combiner_xor
+        pop_size = np.shape(individuals)[0]
+        for i in range(pop_size):
+            yield LTFArray(np.matrix(individuals[i, :]), transform, combiner,
+                           bias=False)
+
+    @staticmethod
+    def get_delay_differences(ltf_arrays, pop_size, challenges, challenge_num):
+        # returns 2D array of delay differences for all challenges on every
+        #   individual
+        delay_diffs = np.empty((pop_size, challenge_num))
+        for i, ltf_array in enumerate(ltf_arrays):
+            for j, challenge in challenges:
+                delay_diffs[i][j] = ltf_array.val(challenge)
+        return delay_diffs
+
+    @staticmethod
+    def get_reliabilities(delay_diffs, epsilon_theo):
+        # returns 2D array of reliabilities for all challenges on every individual
+        reliabilities = np.zeros(np.shape(delay_diffs))
+        indices_of_unreliable = delay_diffs > -epsilon_theo and delay_diffs < epsilon_theo
+        reliabilities[indices_of_unreliable] = 1
+        return reliabilities
+
+    @staticmethod
+    def get_correlations(reliabilities, measured_rels):
+        # returns array of pearson correlation coefficients between reliability
+        #   array of individual and instance for all individuals
+        pop_size = np.shape(reliabilities)[0]
+        correlations = np.empty(pop_size)
+        for i in range(pop_size):
+            correlations[i] = np.corrcoef(reliabilities[i,:], measured_rels)[0, 1]
+        return correlations
+
+    @staticmethod
+    def sort_individuals(individuals, correlations):
+        # returns 2D array of individuals as given from input, but sorted through
+        #   correlation coefficients
+        sorted_indices = np.argsort(correlations)
+        return individuals[sorted_indices[::-1]]
+
+
+    # helping methods
+    @staticmethod
+    def learn_epsilon(instance, unreliability):
+        # returns epsilon on the assumption:
+        #   98% of challenges are reliable on a single PUF,
+        #   while there are 1/2 * ((96%)^k + 1) on k XORed PUFs
         n = instance.n
+        k = instance.k
         num = 1000
         repeat = 100
+        quantile = ((1-2*unreliability)**k + 1) / 2
+        cut = num - int(num * quantile)
         challenges = tools.sample_inputs(n, num)
         ratios = np.zeros(num)
-        responses = np.zeros(num, repeat)
+        responses = np.zeros((num, repeat))
         for i, challenge in enumerate(challenges):
             for j in range(repeat):
                 responses[i, j] = instance.eval(challenge)
             ratio = np.sum(responses[i, :]) / repeat
             ratios[i] = ratio if ratio <= 0.5 else (1 - ratio)
         ratios = np.sort(ratios)
-        return np.sum(ratios[15:25]) / 10
+        return np.sum(ratios[cut-3 : cut+3]) / 10
+
+    @staticmethod
+    def get_epsilon_theo(n, unreliability):
+        return np.sqrt(n) * stats.norm.ppf(0.5 + unreliability/2)
+
+    @staticmethod
+    def set_pole_of_LTFs(instance, different_LTFs):
+        # returns the correctly polarized XOR-LTFArray
+        challenge_num = 100
+        responses = np.empty(2 ** instance.k + 1, challenge_num)
+        for i, permutation in enumerate(itertools.product((-1, 1), repeat=instance.k)):
+            weight_arrays = (different_LTFs.T * permutation).T
+            xor_LTFArray = LTFArray(weight_arrays, LTFArray.combiner_xor,
+                                    LTFArray.transform_id)
+            challenges = tools.sample_inputs(instance.n, challenge_num)
+            for j, challenge in enumerate(challenges):
+                responses[0, j] = instance.eval(challenge)
+                responses[i + 1, j] = xor_LTFArray.eval(challenge)
+        differences = np.empty(np.shape(responses)[0] - 1)
+        for l in range(1, np.shape(responses)[0]):
+            differences[l - 1] = np.sum(np.abs(responses[0, :] - responses[l, :]))
+        sorted_indices = np.argsort(differences)
+        sorted_LTFs = different_LTFs[sorted_indices[::]]
+        return sorted_LTFs[0]
+
+    @staticmethod
+    def is_correlated(responses):
+        (num_of_LTFs, num) = np.shape(responses)
+        for i in range(1, num_of_LTFs):
+            differences = np.sum(np.abs(responses[0, :] - responses[i, :]))
+            if differences < 0.3*num or differences > 0.7*num:
+                return True
+        return False
 
     @staticmethod
     def measure_rels(instance, challenges, challenge_num, repeat, epsilon):
         # returns array of measured reliabilities of instance
         measured_rels = np.zeros(challenge_num)
-        responses = np.zeros(challenge_num, repeat)
+        responses = np.zeros((challenge_num, repeat))
         for i, challenge in enumerate(challenges):
             for j in range(repeat):
                 responses[i, j] = instance.eval(challenge)
@@ -201,55 +302,10 @@ class Reliability_based_CMA_ES():
         return cm_mu
 
     @staticmethod
-    def eigen_decomposition(matrix):
+    def modify_eigen_decomposition(matrix):
         # returns modified eigen-decomposition of matrix A = B * D^2 * B^T
         #   B * D^(-1) * B^T
         eigen_values, eigen_vectors = np.linalg.eigh(matrix)
         diagonal = np.sqrt((np.diag(eigen_values)))
         diagonal_inverse = np.linalg.inv(diagonal)
         return eigen_vectors @ diagonal_inverse @ eigen_vectors.T
-
-    @staticmethod
-    def build_ltf_arrays(individuals):
-        # returns iterator over ltf_arrays created out of every individual
-        transform = LTFArray.transform_id
-        combiner = LTFArray.combiner_xor
-        pop_size = np.shape(individuals)[1]
-        for i in range(pop_size):
-            yield LTFArray(individuals.T[:, i], transform, combiner,
-                           bias=False)
-
-    @staticmethod
-    def get_delay_differences(ltf_arrays, pop_size, challenges, challenge_num):
-        # returns 2D array of delay differences for all challenges on every
-        #   individual
-        delay_diffs = np.empty(pop_size, challenge_num)
-        for i, ltf_array in enumerate(ltf_arrays):
-            for j, challenge in challenges:
-                delay_diffs[i][j] = ltf_array.val(challenge)
-        return delay_diffs
-
-    @staticmethod
-    def get_reliabilities(delay_diffs, epsilon):
-        # returns 2D array of reliabilities for all challenges on every individual
-        reliabilities = np.zeros(np.shape(delay_diffs))
-        indices_of_unreliable = delay_diffs > -epsilon and delay_diffs < epsilon
-        reliabilities[indices_of_unreliable] = 1
-        return reliabilities
-
-    @staticmethod
-    def get_correlations(reliabilities, measured_rels):
-        # returns array of pearson correlation coefficients between reliability
-        #   array of individual and instance for all individuals
-        pop_size = np.shape(reliabilities[0])
-        correlations = np.empty(pop_size)
-        for i in range(pop_size):
-            correlations[i] = np.corrcoef(reliabilities[i,:], measured_rels)[0, 1]
-        return correlations
-
-    @staticmethod
-    def sort_individuals(individuals, correlations):
-        # returns 2D array of individuals as given from input, but sorted through
-        #   correlation coefficients
-        sorted_indices = np.argsort(correlations)
-        return individuals[sorted_indices[::-1]]
