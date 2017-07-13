9ffc53ddf0e0fe3bf4d2944922422c7379e43e16
Nils Wisiol <info@nils-wisiol.de>
2017-07-13 07:48:14 +0200

    Reduced number of CRPs in tests to speed up tests.
    In two tests, the number of CRPs was (way) higher than the total number
    of possible challenges. In one test, the number was higher than it had
    to be.

--- test/test_experimenter.py
+++ test/test_experimenter.py
@@ -6,15 +6,15 @@ from pypuf.experiments.experimenter import Experimenter
 
 class TestExperimenter(unittest.TestCase):
     def test_lr_experiments(self):
-        lr16_4_1 = ExperimentLogisticRegression('exp1.log', 8, 2, 12000, 0xbeef, 0xbeef, LTFArray.transform_id,
+        lr16_4_1 = ExperimentLogisticRegression('exp1.log', 8, 2, 2**8, 0xbeef, 0xbeef, LTFArray.transform_id,
                                                 LTFArray.combiner_xor, restarts=6)
-        lr16_4_2 = ExperimentLogisticRegression('exp2.log', 8, 2, 12000, 0xbeef, 0xbeef, LTFArray.transform_id,
+        lr16_4_2 = ExperimentLogisticRegression('exp2.log', 8, 2, 2**8, 0xbeef, 0xbeef, LTFArray.transform_id,
                                                 LTFArray.combiner_xor, restarts=6)
-        lr16_4_3 = ExperimentLogisticRegression('exp3.log', 8, 2, 12000, 0xbeef, 0xbeef, LTFArray.transform_id,
+        lr16_4_3 = ExperimentLogisticRegression('exp3.log', 8, 2, 2**8, 0xbeef, 0xbeef, LTFArray.transform_id,
                                                 LTFArray.combiner_xor, restarts=6)
-        lr16_4_4 = ExperimentLogisticRegression('exp4.log', 8, 2, 12000, 0xbeef, 0xbeef, LTFArray.transform_id,
+        lr16_4_4 = ExperimentLogisticRegression('exp4.log', 8, 2, 2**8, 0xbeef, 0xbeef, LTFArray.transform_id,
                                                 LTFArray.combiner_xor, restarts=6)
-        lr16_4_5 = ExperimentLogisticRegression('exp5.log', 8, 2, 12000, 0xbeef, 0xbeef, LTFArray.transform_id,
+        lr16_4_5 = ExperimentLogisticRegression('exp5.log', 8, 2, 2**8, 0xbeef, 0xbeef, LTFArray.transform_id,
                                                 LTFArray.combiner_xor, restarts=6)
         experiments = [lr16_4_1, lr16_4_2, lr16_4_3, lr16_4_4, lr16_4_5]
         experimenter = Experimenter('log', experiments)
