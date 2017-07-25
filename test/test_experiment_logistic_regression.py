8a009173a593a5c36a3c25e3dd38d8548236fc6e
Nils Wisiol <info@nils-wisiol.de>
2017-07-25 18:36:17 +0200

    Streamlined Experimenter class, refactored experiment logging
    - Output of Experiment now relies only on python logging
    - Experimenter organizes all results in single file with the result logger
    - Experiment creates an exclusive logfile for progress logging
    - Logistic Regression outputs weights for each step on DEBUG log level to
      progress logger
    - Logistic Regression outputs result on INFO log level to result logger
    - Semaphore logic is now moved to Experimenter entirely

--- test/test_experiment_logistic_regression.py
+++ test/test_experiment_logistic_regression.py
@@ -4,13 +4,7 @@ from pypuf.experiments.experiment.logistic_regression import ExperimentLogisticR
 
 
 class TestExperimentLogisticRegression(unittest.TestCase):
-    def setUp(self):
+    def test_run_and_analyze(self):
         self.lr16_4 = ExperimentLogisticRegression('exp1.log', 8, 2, 2**8, 0xbeef, 0xbeef, LTFArray.transform_id,
-                                                   LTFArray.combiner_xor, restarts=6)
-        self.lr16_4.analysis()
-
-    def test_lr_experiment_output_string(self):
-        self.assertNotEqual(self.lr16_4.output_string(), '')
-
-    def test_lr_experiment_name(self):
-        self.assertNotEqual(self.lr16_4.name, '')
+                                                   LTFArray.combiner_xor)
+        self.lr16_4.execute()
