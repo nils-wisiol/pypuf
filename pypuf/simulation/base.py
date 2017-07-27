c0c59a49e4cefacc5e4ad5289ae5a8ce6f451aa0
nicostephan <nico.stephan@gmx.de>
2017-07-27 12:26:43 +0200

    Delete base.py

--- pypuf/simulation/base.py
+++
@@ -1,8 +0,0 @@
-import abc
-
-
-class Simulation(object, metaclass=abc.ABCMeta):
-
-    @abc.abstractmethod
-    def eval(self):
-        raise NotImplementedError('users must define learn to use this base class')
