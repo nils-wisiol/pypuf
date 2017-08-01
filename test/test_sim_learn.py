ea72b89322602ddedd012b74f3f0bc1f124c0bf0
Benjamin Zengin <b.zengin@yahoo.de>
2017-08-01 18:31:03 +0200

    Adds the original lightweight secure transform from Majzoobi

--- test/test_sim_learn.py
+++ test/test_sim_learn.py
@@ -15,6 +15,9 @@ class TestSimLearn(unittest.TestCase):
     def test_lightweight_secure(self):
         sim_learn.main(["sim_learn", "8", "2", "lightweight_secure", "xor", "20", "1", "2", "1234", "1234"])
 
+    def test_lightweight_secure_original(self):
+        sim_learn.main(["sim_learn", "8", "2", "lightweight_secure_original", "xor", "20", "1", "2", "1234", "1234"])
+
     def test_1_n_bent(self):
         sim_learn.main(["sim_learn", "8", "2", "1_n_bent", "xor", "20", "1", "2", "1234", "1234"])
 
