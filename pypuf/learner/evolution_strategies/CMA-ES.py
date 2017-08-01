f88d98744de57fa618a158088398dc3f62a28741
Chrism7 <chrism@zedat.fu-berlin.de>
2017-08-01 18:49:19 +0200

    seperate CMA-ES from becker

---
+++ pypuf/learner/evolution_strategies/CMA-ES.py
@@ -0,0 +1,129 @@
+import numpy as np
+import scipy.special as sp
+
+class CMA_ES():
+
+    def __init__(self, fitness_function, precision, n, pop_size, parent_size, weights, prng=np.random.RandomState()):
+        self.prng = prng                                        # pseudo random number generator for random mutations
+        self.precision = precision                              # intended scale of step-size to achieve
+        self.evaluate_fitness = fitness_function                # fitness function for evaluating solution candidates
+        self.n = n                                              # number of parameters to learn
+        self.individuals = np.zeros((self.pop_size, self.n))    # solution candidates
+        # mean, step_size,  pop_size,   parent_size,    priorities, cov_matrix,   path_cm,    path_ss
+        # m,    sigma,      lambda,     mu,             w_i,        C,            p_c,        p_sigma
+        self.mean = np.zeros(self.n)                            # mean vector of distribution
+        self.step_size = 1                                      # distance to subsequent mean
+        self.pop_size = pop_size                                # number of individuals per generation
+        self.parent_size = parent_size                          # number of considered individuals
+        self.weights = weights                                  # array of consideration proportion
+        self.cov_matrix = np.identity(self.n)                   # shape of distribution ellipsoid
+        self.path_cm = np.zeros(self.n)                         # cumulated evolution path of covariance matrix
+        self.path_ss = np.zeros(self.n)                         # cumulated evolution path of step size
+        # auxiliary constants
+        self.mu_w = 1 / np.sum(np.square(self.weights))
+        self.c_mu = self.mu_w / self.n**2
+        self.c_1 = 2 / self.n**2
+        self.c_c = 4 / self.n
+        self.c_sigma = 4 / self.n
+        self.c_d_sigma = self.c_sigma / (1 + np.sqrt(self.mu_w / self.n))
+        assert len(self.weights) == self.parent_size
+        assert int(np.ndarray.sum(self.weights)) == 1
+        assert (self.c_1 + self.c_mu <= 1)
+
+    def evolutionary_search(self):
+        # this is the main CMA-ES algorithm like that from Hansen et. al.
+        terminate = False
+        solution = np.zeros(np.shape(self.individuals)[1])
+        estimation_multinormal = np.sqrt(2) * sp.gamma((self.n+1)/2) / sp.gamma((self.n)/2)
+        zero_mean = np.zeros(np.shape(self.mean))
+        while not terminate:
+            if self.step_size < self.precision:
+                terminate = True
+                solution = self.mean
+                break
+            mutations = self.sample_mutations(zero_mean, self.cov_matrix, self.pop_size, self.prng)
+            self.individuals = self.reproduce(self.mean, self.pop_size, self.step_size, mutations)
+            fitness_values = self.evaluate_fitness(self.individuals)
+            count_nan = np.count_nonzero(np.isnan(fitness_values))
+            sorting_indices = np.argsort(fitness_values)
+            sorted_mutations = np.roll(mutations[sorting_indices[::-1]], -count_nan)
+            favorite_mutations = self.get_favorite_mutations(sorted_mutations, self.parent_size, self.weights)
+            self.mean = self.update_mean(self.mean, self.step_size, favorite_mutations)
+            self.path_cm = self.cumulation_for_cm(self.path_cm, self.c_c, self.path_ss, self.n, self.mu_w,
+                                                  favorite_mutations)
+            self.path_ss = self.cumulation_for_ss(self.path_ss, self.c_sigma, self.mu_w, self.cov_matrix,
+                                                  favorite_mutations)
+            cm_mu = self.get_cm_mu(sorted_mutations, self.parent_size, self.weights)
+            self.cov_matrix = self.update_cm(self.cov_matrix, self.c_1, self.c_mu, self.path_cm, cm_mu)
+            self.step_size = self.update_ss(self.step_size, self.c_d_sigma, self.path_ss, estimation_multinormal)
+        return solution
+
+    # updating methods of evolution strategies
+    @staticmethod
+    def sample_mutations(zero_mean, cov_matrix, pop_size, mutation_prng):
+        # returns a new generation of individuals as 2D array (corresponds to y_i)
+        return mutation_prng.multivariate_normal(zero_mean, cov_matrix, pop_size)
+
+    @staticmethod
+    def reproduce(mean, pop_size, step_size, mutations):
+        # returns a new generation of individuals as 2D array (corresponds to x_i)
+        duplicated_mean = np.tile(mean, (pop_size, 1))
+        return duplicated_mean + (step_size * mutations)
+
+    @staticmethod
+    def update_mean(mean, step_size, parent_mutations):
+        # returns mean of a new population as array (corresponds to m)
+        return mean + step_size*parent_mutations
+
+    @staticmethod
+    def cumulation_for_cm(path_cm, c_c, path_ss, n, mu_w, parent_mutations):
+        # returns cumulated evolution path of covariance matrix (corresponds to p_c)
+        path_cm = path_cm * (1-c_c)
+        if(np.linalg.norm(path_ss) < 1.5 * np.sqrt(n)):
+            path_cm = path_cm + (np.sqrt(1 - (1-c_c)**2) * np.sqrt(mu_w) * parent_mutations)
+        return path_cm
+
+    @staticmethod
+    def cumulation_for_ss(path_ss, c_sigma, mu_w, cov_matrix, parent):
+        # returns cumulated evolution path of step-size (corresponds to p_sigma)
+        cm_eigen_dec = __class__.modify_eigen_decomposition(cov_matrix)
+        return (1-c_sigma) * path_ss + np.sqrt(1 - (1-c_sigma)**2) * np.sqrt(mu_w) * cm_eigen_dec @ parent
+
+    @staticmethod
+    def update_cm(cov_matrix, c_1, c_mu, path_cm, cm_mu):
+        # returns covariance matrix of a new population (corresponds to C)
+        return (1 - c_1 - c_mu) * cov_matrix + c_1 * path_cm[:, np.newaxis] @ path_cm[np.newaxis, :] + c_mu * cm_mu
+
+    @staticmethod
+    def update_ss(step_size, c_d_sigma, path_ss, estimation_multinormal):
+        # returns step-size of a new population (corresponds to sigma)
+        factor = np.exp((c_d_sigma) * ((np.linalg.norm(path_ss) / estimation_multinormal) - 1))
+        return step_size * factor
+
+    @staticmethod
+    def get_favorite_mutations(sorted_mutations, parent_size, priorities):
+        # returns the weighted sum of the fittest individuals mutations (corresponds to y_w)
+        parent_mutations = np.zeros(np.shape(sorted_mutations)[1])
+        for i in range(parent_size):
+            parent_mutations = parent_mutations + priorities[i] * sorted_mutations[i, :]
+        return parent_mutations
+
+    @staticmethod
+    def get_cm_mu(sorted_mutations, parent_size, priorities):
+        # returns the weighted sum of the product of the fittest individuals mutations (corresponds to C_mu)
+        cm_mu = np.zeros((np.shape(sorted_mutations)[1], np.shape(sorted_mutations)[1]))
+        for i in range(parent_size):
+            cm_mu = cm_mu + priorities[i] * sorted_mutations[i, :, np.newaxis] @ sorted_mutations[i, np.newaxis, :]
+        return cm_mu
+
+    @staticmethod
+    def modify_eigen_decomposition(matrix):
+        # returns modified eigen-decomposition (B * D^(-1) * B^T) of matrix A = (B * D^2 * B^T) (corresponds to C^(-1/2))
+        eigen_values, eigen_vectors = np.linalg.eigh(matrix)
+        diagonal = 1 / np.diag(np.sqrt(np.diag(eigen_values)))
+        return eigen_vectors @ np.diag(diagonal) @ eigen_vectors.T
+
+    @staticmethod
+    def is_symmetric(matrix, tol=1e-8):
+        # returns true iff the matrix is symmetric
+        return np.allclose(matrix, matrix.T, atol=tol)
--- pypuf/learner/evolution_strategies/test.py
+++ pypuf/learner/evolution_strategies/test.py
@@ -4,6 +4,111 @@ from pypuf import tools
 from pypuf.simulation.arbiter_based.ltfarray import LTFArray, NoisyLTFArray
 from pypuf.learner.evolution_strategies.becker import Reliability_based_CMA_ES as learner
 
+cnt = np.count_nonzero(np.isnan(a))
+c = np.roll(b, -cnt)
+
+a = np.array([  1.,  -1.,   np.nan,  0.,  np.nan], dtype=np.float32)
+print(a)
+cnt = np.count_nonzero(np.isnan(a))
+print('cnt: ', cnt)
+ind = np.argsort(a)
+b = a[ind[::-1]]
+print('b\n', b)
+c = np.roll(b, -cnt)
+print('c: ', c)
+
+
+sa = np.argsort(a)[::-1]
+print(sa)
+asa = np.roll(sa,-np.count_nonzero(np.isnan(a)))
+print(asa)
+
+"""
+def modify_eigen_decomposition(matrix):
+    # returns modified eigen-decomposition (B * D^(-1) * B^T) of matrix A = (B * D^2 * B^T) (corresponds to C^(-1/2))
+    eigen_values, eigen_vectors = np.linalg.eigh(matrix)
+    diagonal = np.sqrt(np.diag(eigen_values))
+    return eigen_vectors[:, np.newaxis] @ (1 / diagonal) @ eigen_vectors[np.newaxis, :]
+"""
+
+
+def modify_eigen_decomposition(matrix):
+    # returns modified eigen-decomposition (B * D^(-1) * B^T) of matrix A = (B * D^2 * B^T) (corresponds to C^(-1/2))
+    eigen_values, eigen_vectors = np.linalg.eigh(matrix)
+    diagonal = 1 / np.diag(np.sqrt(np.diag(eigen_values)))
+    return eigen_vectors @ np.diag(diagonal) @ eigen_vectors.T
+
+"""
+def modify_eigen_decomposition(matrix):
+    # returns modified eigen-decomposition (B * D^(-1) * B^T) of matrix A = (B * D^2 * B^T) (corresponds to C^(-1/2))
+    eigen_values, eigen_vectors = np.linalg.eigh(matrix)
+    print('eigen_values\n', eigen_values, 'eigen_vectors\n', eigen_vectors)
+    diagonal = np.sqrt(np.diag(eigen_values))
+    print('diagonal\n', diagonal)
+    print('eigen_vectors @ (1/diagonal)\n', eigen_vectors @ diagonal**(-1), '@ eigen_vectors.T\n', eigen_vectors @ (1/diagonal) @ eigen_vectors.T)
+    return eigen_vectors @ (1/diagonal) @ eigen_vectors.T
+"""
+
+a = np.array([[2,0], [0,2]])
+#b = 1/a
+#print('bbbbbbbb', b)
+
+mat = np.array([[10,2,3], [2,10,6], [3,6,10]])
+dec = modify_eigen_decomposition(mat)
+print('dec\n', dec)
+
+a = np.array([1,2,3])
+b = np.array([[1,1,1], [1,1,1], [1,1,1]])
+c = a[:, np.newaxis] @ a[np.newaxis, :]
+print('c\n', c)
+c = a@b
+print('c\n', c)
+
+"""
+def modify_eigen_decomposition(matrix):
+    # returns modified eigen-decomposition (B * D^(-1) * B^T) of matrix A = (B * D^2 * B^T) (corresponds to C^(-1/2))
+    eigen_values, eigen_vectors = np.linalg.eigh(matrix)
+    diagonal = np.sqrt(np.diag(eigen_values))
+    diagonal_inverse = np.linalg.inv(diagonal)
+    return eigen_vectors @ diagonal_inverse @ eigen_vectors.T
+"""
+
+def cumulation_for_ss(path_ss, c_sigma, mu_w, cov_matrix, parent):
+    # returns cumulated evolution path of step-size (corresponds to p_sigma)
+    cm_eigen_dec = learner.modify_eigen_decomposition(cov_matrix)
+    return (1 - c_sigma) * path_ss + np.sqrt(1 - (1 - c_sigma) ** 2) * np.sqrt(mu_w) * cm_eigen_dec @ parent
+
+
+path_ss = np.array([-0.34485763, 1.11920498, -0.49091369, -2.78254796, 0.02105892, -5.12057504, 2.60987016, 3.02267444,
+                    -0.19326213, 1.52338899, 0.80677901])
+c_sigma = 0.36363636363636365
+mu_w = 4.0
+cov_matrix = np.array([[0.61842989, 0.42294264, 0.03588551, 0.24631514, 0.24380282, 0.52452589, 0.13642069, 0.1507718, 0.44697789, 0.12312227, 0.9503228],
+                       [0.20485916, 0.93282122, 0.1399231, 0.20006003, 0.22217405, 0.55842256, 0.153686, 0.19383252, 0.49561711, 0.16835053, 0.98860887],
+                       [-0.00146148, 0.3206596, 0.61310816, 0.22702649, 0.21207061, 0.46475151, 0.19473242, 0.14380727, 0.40502616, 0.15206631, 0.67928963],
+                       [0.09919773, 0.2710261, 0.11725606, 0.755224, 0.25845303, 0.55072851, 0.17860856, 0.17158378, 0.43796015, 0.16237649, 0.82313866],
+                       [0.15376438, 0.35021909, 0.15937915, 0.31553199, 0.78021397, 0.5565266, 0.14505443, 0.22198906, 0.44662784, 0.09861747, 0.87262167],
+                       [0.11749206, 0.36947221, 0.09506466, 0.29081208, 0.23953121, 1.23868475, 0.26000033, 0.12934855, 0.53280087, 0.1967795, 0.93000659],
+                       [0.08627627, 0.32162507, 0.18193499, 0.27558156, 0.18494846, 0.61688975, 0.650942, 0.15449631, 0.43014057, 0.08748107, 0.73781146],
+                       [0.10514125, 0.36628546, 0.13552371, 0.27307065, 0.26639696, 0.49075183, 0.15901018, 0.61188589, 0.37888473, 0.12812317, 0.81323803],
+                       [0.16221377, 0.42893648, 0.15760903, 0.30031345, 0.25190217, 0.65507059, 0.19552087, 0.13975116, 1.0348986, 0.22866155, 1.00660005],
+                       [0.07411718, 0.33742892, 0.14040821, 0.26048881, 0.13965083, 0.55480825, 0.0886204, 0.12474863, 0.46442058, 0.66442489, 0.80121933],
+                       [0.38109456, 0.63746411, 0.14740838, 0.40102783, 0.39343187, 0.76781219, 0.21872763, 0.28964033, 0.72213593, 0.28099618, 1.9619393]])
+parent_mutations = np.array([0.59672195, 0.46136144, -0.05686392, 0.00615447, -0.21924931, 0.80377518, -0.0216225, 0.2814175, 0.39674276, 0.24782712, 0.55362269])
+
+eigen_values, eigen_vectors = np.linalg.eigh(cov_matrix)
+print('eigen_values\n', eigen_values, '\neigen_vectors\n', eigen_vectors)
+
+diag = np.diag(eigen_values)
+print('diag\n', diag)
+diagonal = np.sqrt(np.diag(eigen_values))
+print('diagonal\n', diagonal)
+
+print('decomposition\n', learner.modify_eigen_decomposition(cov_matrix))
+
+new_path = cumulation_for_ss(path_ss, c_sigma, mu_w, cov_matrix, parent_mutations)
+print('new_path\n', new_path)
+
 path_ss = np.array([2,2,2,2])
 norm = np.linalg.norm(path_ss)
 print('norm', norm)
@@ -186,3 +291,17 @@ def learn_epsilon(instance, unreliability):
         ratios[i] = ratio if ratio <= 0.5 else (1 - ratio)
     ratios = np.sort(ratios)
     return np.sum(ratios[cut-3 : cut+3]) / 10
+
+"""
+@staticmethod
+def fitness(challenges, challenge_num, measured_rels, individuals):
+    # returns individuals sorted by their correlation coefficient as fitness
+    pop_size = np.shape(individuals)[0]
+    built_LTFArrays = __class__.build_LTFArrays(individuals[:, :-1])
+    delay_diffs = __class__.get_delay_differences(built_LTFArrays, pop_size, challenges, challenge_num)
+    epsilons = individuals[:, -1]
+    reliabilities = __class__.get_reliabilities(delay_diffs, epsilons)
+    correlations = __class__.get_correlations(reliabilities, measured_rels)
+    correlations = [-.3 if x != x else x for x in correlations]
+    return correlations
+"""
