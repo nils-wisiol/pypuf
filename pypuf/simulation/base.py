8d0e8a49b6bfc0ed87723ef40960f3234457881a
Benjamin Zengin <b.zengin@yahoo.de>
2017-08-17 12:07:12 +0200

    Fixes Simulation base interface

--- pypuf/simulation/base.py
+++ pypuf/simulation/base.py
@@ -4,5 +4,5 @@ import abc
 class Simulation(object, metaclass=abc.ABCMeta):
 
     @abc.abstractmethod
-    def eval(self):
+    def eval(self, inputs):
         raise NotImplementedError('users must define learn to use this base class')
