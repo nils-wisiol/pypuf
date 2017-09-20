8b47a630febe58990a9e58d7143cba41fc54b617
Chris Grab <christoph.graebnitz@gmail.com>
2017-09-20 15:35:49 +0200

    fix linter messages ./

--- example.py
+++ example.py
@@ -1,13 +1,17 @@
+"""
+This module is only used to show some example usage of the framework.
+"""
 from pypuf import tools
 from pypuf.learner.regression.logistic_regression import LogisticRegression
 from pypuf.simulation.arbiter_based.ltfarray import LTFArray
+
 
 def main():
     """
     Run an example how to use pypuf.
     Developers Notice: Changes here need to be mirrored to README!
     """
-    
+
     # create a simulation with random (Gaussian) weights
     # for 64-bit 4-XOR
     instance = LTFArray(
