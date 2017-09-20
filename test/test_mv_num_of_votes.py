b7111bc69ab1dc4f798e5245d2b330440c96cec6
Chris Grab <christoph.graebnitz@gmail.com>
2017-09-20 14:41:33 +0200

    fix linter messages test

--- test/test_mv_num_of_votes.py
+++ test/test_mv_num_of_votes.py
@@ -1,10 +1,19 @@
+"""
+This module is used to test the command line tool which searches the number of votes for several instances of
+SimulationMajorityLTFArrays in order to satisfy a overall desired stability.
+"""
 import unittest
 import mv_num_of_votes
 
 
 class TestMvNumOfVotes(unittest.TestCase):
+    """This class is used to test the mv_num_of_votes commandline tool"""
 
     def test_8_1_puf(self):
+        """
+        This method checks the output log of mv_num_of_votes for a stability greater equal the
+        overall_desired_stability.
+        """
         overall_desired_stability = 0.8
         mv_num_of_votes.main(["0.95", str(overall_desired_stability), "8", "1", "1", "0.33", "250", "1"])
 
@@ -17,4 +26,4 @@ class TestMvNumOfVotes(unittest.TestCase):
                 break
 
         # if the line is '' then no stability where found which satisfy overall_desired_stability
-        self.assertNotEquals(line, '')
+        self.assertNotEqual(line, '', 'stability where found which satisfy overall_desired_stability')
