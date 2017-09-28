bdaf9bd219b61ac454266b4fb7ef0ca0813e2e05
Nils Wisiol <info@nils-wisiol.de>
2017-09-28 14:11:51 +0800

    fixed README example and included example code file for testing
    Fixes #70

---
+++ example.py
@@ -0,0 +1,36 @@
+from pypuf import tools
+from pypuf.learner.regression.logistic_regression import LogisticRegression
+from pypuf.simulation.arbiter_based.ltfarray import LTFArray
+
+def main():
+    """
+    Run an example how to use pypuf.
+    Developers Notice: Changes here need to be mirrored to README!
+    """
+    
+    # create a simulation with random (Gaussian) weights
+    # for 64-bit 4-XOR
+    instance = LTFArray(
+        weight_array=LTFArray.normal_weights(n=64, k=2),
+        transform=LTFArray.transform_atf,
+        combiner=LTFArray.combiner_xor,
+    )
+
+    # create the learner
+    lr_learner = LogisticRegression(
+        t_set=tools.TrainingSet(instance=instance, N=12000),
+        n=64,
+        k=2,
+        transformation=LTFArray.transform_atf,
+        combiner=LTFArray.combiner_xor,
+    )
+
+    # learn and test the model
+    model = lr_learner.learn()
+    accuracy = 1 - tools.approx_dist(instance, model, 10000)
+
+    # output the result
+    print('Learned a 64bit 2-xor XOR Arbiter PUF from 12000 CRPs with accuracy %f' % accuracy)
+
+if __name__ == '__main__':
+    main()
