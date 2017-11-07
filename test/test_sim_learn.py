7de8379d3bbeb7b3af4da11108042423f60c3d2e
Chris Grab <christoph.graebnitz@gmail.com>
2017-11-07 16:18:14 +0100

    rework test_sim_learn
    This changes the log path to "test/".

--- test/test_sim_learn.py
+++ test/test_sim_learn.py
@@ -1,8 +1,6 @@
 """This module tests the sim_learn command line script."""
 import unittest
-import os
-import glob
-from test.utility import mute
+from test.utility import remove_test_logs, LOG_PATH, mute
 import sim_learn
 
 
@@ -12,91 +10,98 @@ class TestSimLearn(unittest.TestCase):
     """
     def setUp(self):
         # Remove all log files
-        paths = list(glob.glob('*.log'))
-        for path in paths:
-            os.remove(path)
+        remove_test_logs()
 
     def tearDown(self):
         # Remove all log files
-        paths = list(glob.glob('*.log'))
-        for path in paths:
-            os.remove(path)
+        remove_test_logs()
 
     @mute
     def test_id(self):
         """This tests the id transformation and xor combiner."""
-        sim_learn.main(["sim_learn", "8", "2", "id", "xor", "20", "1", "2", "1234", "1234"])
+        sim_learn.main(["sim_learn", "8", "2", "id", "xor", "20", "1", "2", "1234", "1234", LOG_PATH + "test_id"])
 
     @mute
     def test_atf(self):
         """This tests the atf transformation and xor combiner."""
-        sim_learn.main(["sim_learn", "8", "2", "atf", "xor", "20", "1", "2", "1234", "1234"])
+        sim_learn.main(["sim_learn", "8", "2", "atf", "xor", "20", "1", "2", "1234", "1234", LOG_PATH + "test_atf"])
 
     @mute
     def test_mm(self):
         """This tests the mm transformation and xor combiner."""
-        sim_learn.main(["sim_learn", "8", "2", "mm", "xor", "20", "1", "2", "1234", "1234"])
+        sim_learn.main(["sim_learn", "8", "2", "mm", "xor", "20", "1", "2", "1234", "1234", LOG_PATH + "test_mm"])
 
     @mute
     def test_lightweight_secure(self):
         """This tests the lightweight secure transformation and xor combiner."""
-        sim_learn.main(["sim_learn", "8", "2", "lightweight_secure", "xor", "20", "1", "2", "1234", "1234"])
+        sim_learn.main(["sim_learn", "8", "2", "lightweight_secure", "xor", "20", "1", "2", "1234", "1234",
+                        LOG_PATH + "test_lightweight_secure"])
 
     @mute
     def test_lightweight_secure_original(self):
         """This tests the lightweight secure original transformation and xor combiner."""
-        sim_learn.main(["sim_learn", "8", "2", "lightweight_secure_original", "xor", "20", "1", "2", "1234", "1234"])
+        sim_learn.main(["sim_learn", "8", "2", "lightweight_secure_original", "xor", "20", "1", "2", "1234", "1234",
+                        LOG_PATH+"test_lightweight_secure_original"])
 
     @mute
     def test_1_n_bent(self):
         """This tests the one to n bent transformation and xor combiner."""
-        sim_learn.main(["sim_learn", "8", "2", "1_n_bent", "xor", "20", "1", "2", "1234", "1234"])
+        sim_learn.main(["sim_learn", "8", "2", "1_n_bent", "xor", "20", "1", "2", "1234", "1234",
+                        LOG_PATH + "test_1_n_bent"])
 
     @mute
     def test_1_1_bent(self):
         """This tests the one to one bent transformation and xor combiner."""
-        sim_learn.main(["sim_learn", "8", "2", "1_1_bent", "xor", "20", "1", "2", "1234", "1234"])
+        sim_learn.main(["sim_learn", "8", "2", "1_1_bent", "xor", "20", "1", "2", "1234", "1234",
+                        LOG_PATH + "test_1_1_bent"])
 
     @mute
     def test_ip_mod2_id(self):
         """This tests the identity transformation and inner product mod 2 combiner."""
-        sim_learn.main(["sim_learn", "8", "2", "id", "ip_mod2", "20", "1", "2", "1234", "1234"])
+        sim_learn.main(["sim_learn", "8", "2", "id", "ip_mod2", "20", "1", "2", "1234", "1234",
+                        LOG_PATH + "test_ip_mod2_id"])
 
     @mute
     def test_ip_mod2_atf(self):
         """This tests the atf transformation and inner product mod 2 combiner."""
-        sim_learn.main(["sim_learn", "8", "2", "atf", "ip_mod2", "20", "1", "2", "1234", "1234"])
+        sim_learn.main(["sim_learn", "8", "2", "atf", "ip_mod2", "20", "1", "2", "1234", "1234",
+                        LOG_PATH + "test_ip_mod2_atf"])
 
     @mute
     def test_ip_mod2_mm(self):
         """This tests the mm transformation and inner product mod 2 combiner."""
-        sim_learn.main(["sim_learn", "8", "2", "mm", "ip_mod2", "20", "1", "2", "1234", "1234"])
+        sim_learn.main(["sim_learn", "8", "2", "mm", "ip_mod2", "20", "1", "2", "1234", "1234",
+                        LOG_PATH + "test_ip_mod2_mm"])
 
     @mute
     def test_ip_mod2_lightweight_secure(self):
         """This tests the lightweight secure transformation and inner product mod 2 combiner."""
-        sim_learn.main(["sim_learn", "8", "2", "lightweight_secure", "ip_mod2", "20", "1", "2", "1234", "1234"])
+        sim_learn.main(["sim_learn", "8", "2", "lightweight_secure", "ip_mod2", "20", "1", "2", "1234", "1234",
+                        LOG_PATH + "test_ip_mod2_lightweight_secure"])
 
     @mute
     def test_ip_mod2_1_n_bent(self):
         """This tests the one to n bent transformation and inner product mod 2 combiner."""
-        sim_learn.main(["sim_learn", "8", "2", "1_n_bent", "ip_mod2", "20", "1", "2", "1234", "1234"])
+        sim_learn.main(["sim_learn", "8", "2", "1_n_bent", "ip_mod2", "20", "1", "2", "1234", "1234",
+                        LOG_PATH + "test_ip_mod2_1_n_bent"])
 
     @mute
     def test_ip_mod2_1_1_bent(self):
         """This tests the one to one bent transformation and inner product mod 2 combiner."""
-        sim_learn.main(["sim_learn", "8", "2", "1_1_bent", "ip_mod2", "20", "1", "2", "1234", "1234"])
+        sim_learn.main(["sim_learn", "8", "2", "1_1_bent", "ip_mod2", "20", "1", "2", "1234", "1234",
+                        LOG_PATH+"test_ip_mod2_1_1_bent"])
 
     @mute
     def test_permutation_atf(self):
         """This tests the atf permutation transformation and xor combiner."""
-        sim_learn.main(["sim_learn", "8", "2", "permutation_atf", "xor", "10", "1", "2", "1234", "1234"])
+        sim_learn.main(["sim_learn", "8", "2", "permutation_atf", "xor", "10", "1", "2", "1234", "1234",
+                        LOG_PATH + "test_permutation_atf"])
 
     @mute
     def test_log_name(self):
         """This tests for the expected number of lines in the main log file."""
         instance_count = 2
-        log_name = 'test_log'
+        log_name = LOG_PATH + 'test_log_name'
         sim_learn.main(["sim_learn", "8", "2", "id", "xor", "10", "1", str(instance_count), "1234", "1234", log_name])
 
         def line_count(file_object):
@@ -115,14 +120,14 @@ class TestSimLearn(unittest.TestCase):
         log_file.close()
 
     @mute
-    def test_number_of_reults(self):
+    def test_number_of_results(self):
         """
         This test checks the number of results to match a previous calculated value.
         """
         instance_count = 7
         restarts = 13
         expected_number_of_result = instance_count * restarts
-        log_name = 'test_log'
+        log_name = LOG_PATH + 'test_number_of_results'
         sim_learn.main(
             ["sim_learn", "8", "2", "id", "xor", "10", str(restarts), str(instance_count), "1234", "1234", log_name]
         )
