8a0d7d954cf327dca4450a5c632d94218330de21
Chris Grab <christoph.graebnitz@gmail.com>
2017-11-08 17:03:18 +0100

    add mute wrapper to verbose tests

--- test/test_example.py
+++ test/test_example.py
@@ -2,6 +2,7 @@
 This module ensures that the example code is running with the current version of the framework.
 """
 import unittest
+from test.utility import mute
 import example
 
 
@@ -9,6 +10,7 @@ class TestExample(unittest.TestCase):
     """
     This class is used to execute the code examples.
     """
+    @mute
     def test_default(self):
         """
         This method just runs the example code in order to test for runtime errors.
