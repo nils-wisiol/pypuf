0127a71085d78510abcefb04a2931affb1bd6b00
Chris Grab <christoph.graebnitz@gmail.com>
2017-11-08 16:58:01 +0100

    fix unclosed file bug

--- test/test_mv_num_of_votes.py
+++ test/test_mv_num_of_votes.py
@@ -18,12 +18,12 @@ class TestMvNumOfVotes(unittest.TestCase):
         mv_num_of_votes.main(["0.95", str(overall_desired_stability), "8", "1", "1", "0.33", "250", "1"])
 
         # Check if the number of results is correct
-        log_file = open('exp1.0xc0deba5e_0_8_1_250_transform_id_combiner_xor.log', 'r')
-        line = log_file.readline()
-        while line != '':
-            stability = float(line.split('\t')[0])
-            if stability >= overall_desired_stability:
-                break
+        with open('exp1.0xc0deba5e_0_8_1_250_transform_id_combiner_xor.log', 'r') as log_file:
+            line = log_file.readline()
+            while line != '':
+                stability = float(line.split('\t')[0])
+                if stability >= overall_desired_stability:
+                    break
 
         # if the line is '' then no stability where found which satisfy overall_desired_stability
         self.assertNotEqual(line, '', 'stability where found which satisfy overall_desired_stability')
