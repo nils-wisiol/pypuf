ee4c865cc8755d587c5b75ee94e07039322e5538
Chris Grab <christoph.graebnitz@gmail.com>
2017-11-08 17:03:18 +0100

    add mute wrapper to verbose tests

--- test/test_sim_learn.py
+++ test/test_sim_learn.py
@@ -2,6 +2,7 @@
 import unittest
 import os
 import glob
+from test.utility import mute
 import sim_learn
 
 
@@ -21,62 +22,77 @@ class TestSimLearn(unittest.TestCase):
         for path in paths:
             os.remove(path)
 
+    @mute
     def test_id(self):
         """This tests the id transformation and xor combiner."""
         sim_learn.main(["sim_learn", "8", "2", "id", "xor", "20", "1", "2", "1234", "1234"])
 
+    @mute
     def test_atf(self):
         """This tests the atf transformation and xor combiner."""
         sim_learn.main(["sim_learn", "8", "2", "atf", "xor", "20", "1", "2", "1234", "1234"])
 
+    @mute
     def test_mm(self):
         """This tests the mm transformation and xor combiner."""
         sim_learn.main(["sim_learn", "8", "2", "mm", "xor", "20", "1", "2", "1234", "1234"])
 
+    @mute
     def test_lightweight_secure(self):
         """This tests the lightweight secure transformation and xor combiner."""
         sim_learn.main(["sim_learn", "8", "2", "lightweight_secure", "xor", "20", "1", "2", "1234", "1234"])
 
+    @mute
     def test_lightweight_secure_original(self):
         """This tests the lightweight secure original transformation and xor combiner."""
         sim_learn.main(["sim_learn", "8", "2", "lightweight_secure_original", "xor", "20", "1", "2", "1234", "1234"])
 
+    @mute
     def test_1_n_bent(self):
         """This tests the one to n bent transformation and xor combiner."""
         sim_learn.main(["sim_learn", "8", "2", "1_n_bent", "xor", "20", "1", "2", "1234", "1234"])
 
+    @mute
     def test_1_1_bent(self):
         """This tests the one to one bent transformation and xor combiner."""
         sim_learn.main(["sim_learn", "8", "2", "1_1_bent", "xor", "20", "1", "2", "1234", "1234"])
 
+    @mute
     def test_ip_mod2_id(self):
         """This tests the identity transformation and inner product mod 2 combiner."""
         sim_learn.main(["sim_learn", "8", "2", "id", "ip_mod2", "20", "1", "2", "1234", "1234"])
 
+    @mute
     def test_ip_mod2_atf(self):
         """This tests the atf transformation and inner product mod 2 combiner."""
         sim_learn.main(["sim_learn", "8", "2", "atf", "ip_mod2", "20", "1", "2", "1234", "1234"])
 
+    @mute
     def test_ip_mod2_mm(self):
         """This tests the mm transformation and inner product mod 2 combiner."""
         sim_learn.main(["sim_learn", "8", "2", "mm", "ip_mod2", "20", "1", "2", "1234", "1234"])
 
+    @mute
     def test_ip_mod2_lightweight_secure(self):
         """This tests the lightweight secure transformation and inner product mod 2 combiner."""
         sim_learn.main(["sim_learn", "8", "2", "lightweight_secure", "ip_mod2", "20", "1", "2", "1234", "1234"])
 
+    @mute
     def test_ip_mod2_1_n_bent(self):
         """This tests the one to n bent transformation and inner product mod 2 combiner."""
         sim_learn.main(["sim_learn", "8", "2", "1_n_bent", "ip_mod2", "20", "1", "2", "1234", "1234"])
 
+    @mute
     def test_ip_mod2_1_1_bent(self):
         """This tests the one to one bent transformation and inner product mod 2 combiner."""
         sim_learn.main(["sim_learn", "8", "2", "1_1_bent", "ip_mod2", "20", "1", "2", "1234", "1234"])
 
+    @mute
     def test_permutation_atf(self):
         """This tests the atf permutation transformation and xor combiner."""
         sim_learn.main(["sim_learn", "8", "2", "permutation_atf", "xor", "10", "1", "2", "1234", "1234"])
 
+    @mute
     def test_log_name(self):
         """This tests for the expected number of lines in the main log file."""
         instance_count = 2
@@ -98,6 +114,7 @@ class TestSimLearn(unittest.TestCase):
         self.assertEqual(line_count(log_file), instance_count, 'Unexpected number of results')
         log_file.close()
 
+    @mute
     def test_number_of_reults(self):
         """
         This test checks the number of results to match a previous calculated value.
