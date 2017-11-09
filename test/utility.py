a73a4eb77ca5008bc07a486cc544c9ed20085f31
Chris Grab <christoph.graebnitz@gmail.com>
2017-11-09 09:36:15 +0100

    add stream redirect contextmanager
    This context mangager is copied from python 3.5 contextlib,
    because it is not implemented in python 3.4.
    The context manager can be used to redirect streams of
    sys.stdout/ sys.stderr in a function scope to an other file.

--- test/utility.py
+++ test/utility.py
@@ -4,6 +4,8 @@ This module provides some utility functions in oder to minimize duplicate code i
 import os
 import glob
 import multiprocessing
+import sys
+from io import StringIO
 from functools import wraps
 from pypuf.experiments.experimenter import log_listener, setup_logger
 
@@ -98,3 +100,72 @@ def get_functions_with_prefix(prefix, obj):
     :return list of function objects
     """
     return [getattr(obj, func) for func in dir(obj) if func.startswith(prefix)]
+
+
+class _RedirectStream:
+    """
+    Context manager for temporarily redirecting a stream to another file.
+    Copied from py35 contextlib because _RedirectStream is not available in py34 contextlib.
+    """
+    _stream = None
+
+    def __init__(self, new_target):
+        self._new_target = new_target
+        # We use a list of old targets to make this CM re-entrant
+        self._old_targets = []
+
+    def __enter__(self):
+        self._old_targets.append(getattr(sys, self._stream))
+        setattr(sys, self._stream, self._new_target)
+        return self._new_target
+
+    def __exit__(self, exctype, excinst, exctb):
+        setattr(sys, self._stream, self._old_targets.pop())
+
+
+class RedirectStdout(_RedirectStream):
+    """
+    Context manager for temporarily redirecting stdout to another file.
+    Copied from py35 contextlib because RedirectStdout is not available in py34 contextlib.
+    """
+
+    _stream = "stdout"
+
+
+class RedirectStderr(_RedirectStream):
+    """
+    Context manager for temporarily redirecting stderr to another file.
+    Copied from py35 contextlib because RedirectStderr is not available in py34 contextlib.
+    """
+
+    _stream = "stderr"
+
+
+def mute(function):
+    """
+    This function provides an function decorator which redirects the stderr and stdout stream to an stream object
+    in order to prevent console output.
+    """
+    def _mute(*args, **kwargs):
+        """
+        This method absorbs stderr and stdout streams.
+        :param args: extra positional generic arguments
+        :param kwargs: extra keyword arguments generic argument
+        :return: return value of `function`
+        """
+        output_sink = StringIO()
+        try:
+            from contextlib import redirect_stdout, redirect_stderr
+            redirect_out = redirect_stdout(output_sink)
+            redirect_err = redirect_stderr(output_sink)
+        except ImportError:
+            redirect_out = RedirectStdout(output_sink)
+            redirect_err = RedirectStderr(output_sink)
+        try:
+            # with redirect_stdout(output_sink):
+            with redirect_err, redirect_out:
+                result = function(*args, **kwargs)
+        except Exception as exception:
+            raise exception
+        return result
+    return wraps(function)(_mute)
