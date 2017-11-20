98e15a2b4417086614db89e6a214ec1ea44639bd
Chris Grab <christoph.graebnitz@gmail.com>
2017-11-20 14:39:45 +0100

    fix sim_learn

--- test/test_sim_learn.py
+++ test/test_sim_learn.py
@@ -17,80 +17,66 @@ class TestSimLearn(unittest.TestCase):
         # Remove all log files
         remove_test_logs()
 
+    def log_parameter(self, name):
+        """
+        This function returns a the log parameter compatible with sim_learn.
+        :param name: string
+                     name of the logfile
+        """
+        return '--log_name={0}'.format(LOG_PATH + name)
+
     @mute
     def test_id(self):
         """This tests the id transformation and xor combiner."""
-        sim_learn.main(["8", "2", "id", "xor", "20", "1", "2", "1234", "1234", LOG_PATH + "test_id"])
+        sim_learn.main(["8", "2", "id", "xor", "20", "1", "2", "1234", "1234", self.log_parameter("test_id")])
 
     @mute
     def test_atf(self):
         """This tests the atf transformation and xor combiner."""
-        sim_learn.main(["8", "2", "atf", "xor", "20", "1", "2", "1234", "1234", LOG_PATH + "test_atf"])
+        sim_learn.main(["8", "2", "atf", "xor", "20", "1", "2", "1234", "1234", self.log_parameter("test_atf")])
+
+    @mute
+    def test_mm(self):
+        """This tests the mm transformation and xor combiner."""
+        sim_learn.main(["8", "2", "mm", "xor", "20", "1", "2", "1234", "1234", self.log_parameter("test_mm")])
 
     @mute
     def test_lightweight_secure(self):
         """This tests the lightweight secure transformation and xor combiner."""
         sim_learn.main(["8", "2", "lightweight_secure", "xor", "20", "1", "2", "1234", "1234",
-                        LOG_PATH+"test_lightweight_secure"])
+                        self.log_parameter("test_lightweight_secure")])
 
     @mute
     def test_ip_mod2_id(self):
         """This tests the identity transformation and inner product mod 2 combiner."""
-        sim_learn.main(["8", "2", "id", "ip_mod2", "20", "1", "2", "1234", "1234",
-                        LOG_PATH + "test_ip_mod2_id"])
+        sim_learn.main(
+            ["8", "2", "id", "ip_mod2", "20", "1", "2", "1234", "1234", self.log_parameter("test_ip_mod2_id")])
 
     @mute
     def test_ip_mod2_atf(self):
         """This tests the atf transformation and inner product mod 2 combiner."""
-        sim_learn.main(["8", "2", "atf", "ip_mod2", "20", "1", "2", "1234", "1234",
-                        LOG_PATH + "test_ip_mod2_atf"])
+        sim_learn.main(
+            ["8", "2", "atf", "ip_mod2", "20", "1", "2", "1234", "1234", self.log_parameter("test_ip_mod2_atf")])
 
     @mute
     def test_ip_mod2_lightweight_secure(self):
         """This tests the lightweight secure transformation and inner product mod 2 combiner."""
         sim_learn.main(["8", "2", "lightweight_secure", "ip_mod2", "20", "1", "2", "1234", "1234",
-                        LOG_PATH + "test_ip_mod2_lightweight_secure"])
+                        self.log_parameter("test_ip_mod2_lightweight_secure")])
 
     @mute
     def test_permutation_atf(self):
         """This tests the atf permutation transformation and xor combiner."""
         sim_learn.main(["8", "2", "permutation_atf", "xor", "10", "1", "2", "1234", "1234",
-                        LOG_PATH + "test_permutation_atf"])
+                        self.log_parameter("test_permutation_atf")])
 
     @mute
     def test_log_name(self):
         """This tests for the expected number of lines in the main log file."""
         instance_count = 2
-        log_name = LOG_PATH + 'test_log_name'
-        sim_learn.main(["8", "2", "id", "xor", "10", "1", str(instance_count), "1234", "1234", log_name])
-
-        def line_count(file_object):
-            """
-            :param file_object:
-            :return: number of lines
-            """
-            count = 0
-            while file_object.readline() != '':
-                count = count + 1
-            return count
-
-        # Check if the number of results is correct
-        log_file = open(log_name + '.log', 'r')
-        self.assertEqual(line_count(log_file), instance_count, 'Unexpected number of results')
-        log_file.close()
-
-    @mute
-    def test_number_of_results(self):
-        """
-        This test checks the number of results to match a previous calculated value.
-        """
-        instance_count = 7
-        restarts = 13
-        expected_number_of_result = instance_count * restarts
-        log_name = LOG_PATH + 'test_number_of_results'
+        log_name = "test_log_name"
         sim_learn.main(
-            ["8", "2", "id", "xor", "10", str(restarts), str(instance_count), "1234", "1234",
-             '--log_name={0}'.format(log_name)]
+            ["8", "2", "id", "xor", "10", "1", str(instance_count), "1234", "1234", self.log_parameter(log_name)]
         )
 
         def line_count(file_object):
@@ -104,13 +90,97 @@ class TestSimLearn(unittest.TestCase):
             return count
 
         # Check if the number of results is correct
-        log_file = open(log_name + '.log', 'r')
+        log_file = open(LOG_PATH + log_name + '.log', 'r')
+        self.assertEqual(line_count(log_file), instance_count, 'Unexpected number of results')
+        log_file.close()
+
+    @mute
+    def test_number_of_results(self):
+        """
+        This test checks the number of results to match a previous calculated value.
+        """
+        instance_count = 7
+        restarts = 13
+        expected_number_of_result = instance_count * restarts
+        log_name = 'test_number_of_results'
+        sim_learn.main(
+            ["8", "2", "id", "xor", "10", str(restarts), str(instance_count), "1234", "1234",
+             self.log_parameter(log_name)]
+        )
+
+        def line_count(file_object):
+            """
+            :param file_object:
+            :return: number of lines
+            """
+            count = 0
+            while file_object.readline() != '':
+                count = count + 1
+            return count
+
+        # Check if the number of results is correct
+        log_file = open(LOG_PATH + log_name + '.log', 'r')
         self.assertEqual(line_count(log_file), expected_number_of_result, 'Unexpected number of results')
         log_file.close()
 
-    def test_seed_challenges(self):
-        """This test the trainingset challenge generation to be deterministic with different seeds."""
-        log_name = 'test/test_log'
-        log_parameter = '--log_name={0}'.format(log_name)
-        seed_parameter = '--seed_challenges=0xBA11'
-        sim_learn.main(["8", "2", "id", "xor", "20", "1", "2", "1234", "1234", log_parameter, seed_parameter])
+    def read_log(self, path):
+        """This function reads the content of a log file without time measuring and deletes the file.
+        :param path: string
+                     Path to a file
+        :return: list of list of string
+        """
+        lines = []
+        with open(path, 'r') as file:
+            line = file.readline()
+            while line != '':
+                values = line.split('\t')
+                # Drop the time value
+                del values[9]
+                lines.append(values)
+                line = file.readline()
+        remove_test_logs(path)
+        return lines
+
+    def check_seeds(self, parameter_set1, parameter_set2, log_path):
+        """
+        This method checks the results of sim_learn for different parameter lists.
+        :param parameter_set1: list of string
+        :param parameter_set2: list of string
+        :param log_path: string
+        """
+        sim_learn.main(parameter_set1)
+        res_param_set1 = self.read_log(log_path)
+
+        sim_learn.main(parameter_set2)
+        res_param_set2 = self.read_log(log_path)
+
+        # Test challenge seed impact
+        self.assertNotEqual(res_param_set1, res_param_set2)
+
+        sim_learn.main(parameter_set1)
+        res_param_set1_2 = self.read_log(log_path)
+
+        sim_learn.main(parameter_set2)
+        res_param_set2_2 = self.read_log(log_path)
+
+        # Test challenge to be deterministic
+        self.assertEqual(res_param_set2, res_param_set2_2)
+        self.assertEqual(res_param_set1, res_param_set1_2)
+
+    @mute
+    def test_seeds(self):
+        """
+        This test the trainingset challenge generation and accuracy seeds.
+        Whit this seeds sim_learn should be deterministic.
+        """
+        log_name = 'test_seed_challenges'
+        log_path = LOG_PATH + log_name + '.log'
+        parameter = ["8", "2", "id", "xor", "20", "1", "2", "1234", "1234", self.log_parameter(log_name)]
+        seed_parameter_chl = '--seed_challenges=0xBA11'
+        seed_parameter_dist = '--seed_distance=5ACE'
+        parameter_with_seed_chl = parameter + [seed_parameter_chl]
+        parameter_with_seed_dist = parameter + [seed_parameter_dist]
+
+        self.check_seeds(parameter, parameter_with_seed_chl, log_path)
+        self.check_seeds(parameter, parameter_with_seed_dist, log_path)
+        self.check_seeds(parameter_with_seed_chl, parameter_with_seed_dist, log_path)
