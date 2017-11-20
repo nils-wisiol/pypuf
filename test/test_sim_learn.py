fda684ec4965cd5628b3f45f37a8605ccd46c175
Chris Grab <christoph.graebnitz@gmail.com>
2017-11-20 11:56:37 +0100

    rework test_sim_learn.py

--- test/test_sim_learn.py
+++ test/test_sim_learn.py
@@ -8,6 +8,7 @@ class TestSimLearn(unittest.TestCase):
     """
     This class is used to test different parameters for the sim_learn module.
     """
+
     def setUp(self):
         # Remove all log files
         remove_test_logs()
@@ -19,41 +20,41 @@ class TestSimLearn(unittest.TestCase):
     @mute
     def test_id(self):
         """This tests the id transformation and xor combiner."""
-        sim_learn.main(["sim_learn", "8", "2", "id", "xor", "20", "1", "2", "1234", "1234", LOG_PATH + "test_id"])
+        sim_learn.main(["8", "2", "id", "xor", "20", "1", "2", "1234", "1234", LOG_PATH + "test_id"])
 
     @mute
     def test_atf(self):
         """This tests the atf transformation and xor combiner."""
-        sim_learn.main(["sim_learn", "8", "2", "atf", "xor", "20", "1", "2", "1234", "1234", LOG_PATH + "test_atf"])
+        sim_learn.main(["8", "2", "atf", "xor", "20", "1", "2", "1234", "1234", LOG_PATH + "test_atf"])
 
     @mute
     def test_lightweight_secure(self):
-        """This tests the lightweight secure original transformation and xor combiner."""
-        sim_learn.main(["sim_learn", "8", "2", "lightweight_secure", "xor", "20", "1", "2", "1234", "1234",
-                        LOG_PATH+"test_lightweight_secure_original"])
+        """This tests the lightweight secure transformation and xor combiner."""
+        sim_learn.main(["8", "2", "lightweight_secure", "xor", "20", "1", "2", "1234", "1234",
+                        LOG_PATH+"test_lightweight_secure"])
 
     @mute
     def test_ip_mod2_id(self):
         """This tests the identity transformation and inner product mod 2 combiner."""
-        sim_learn.main(["sim_learn", "8", "2", "id", "ip_mod2", "20", "1", "2", "1234", "1234",
+        sim_learn.main(["8", "2", "id", "ip_mod2", "20", "1", "2", "1234", "1234",
                         LOG_PATH + "test_ip_mod2_id"])
 
     @mute
     def test_ip_mod2_atf(self):
         """This tests the atf transformation and inner product mod 2 combiner."""
-        sim_learn.main(["sim_learn", "8", "2", "atf", "ip_mod2", "20", "1", "2", "1234", "1234",
+        sim_learn.main(["8", "2", "atf", "ip_mod2", "20", "1", "2", "1234", "1234",
                         LOG_PATH + "test_ip_mod2_atf"])
 
     @mute
     def test_ip_mod2_lightweight_secure(self):
         """This tests the lightweight secure transformation and inner product mod 2 combiner."""
-        sim_learn.main(["sim_learn", "8", "2", "lightweight_secure", "ip_mod2", "20", "1", "2", "1234", "1234",
+        sim_learn.main(["8", "2", "lightweight_secure", "ip_mod2", "20", "1", "2", "1234", "1234",
                         LOG_PATH + "test_ip_mod2_lightweight_secure"])
 
     @mute
     def test_permutation_atf(self):
         """This tests the atf permutation transformation and xor combiner."""
-        sim_learn.main(["sim_learn", "8", "2", "permutation_atf", "xor", "10", "1", "2", "1234", "1234",
+        sim_learn.main(["8", "2", "permutation_atf", "xor", "10", "1", "2", "1234", "1234",
                         LOG_PATH + "test_permutation_atf"])
 
     @mute
@@ -61,7 +62,7 @@ class TestSimLearn(unittest.TestCase):
         """This tests for the expected number of lines in the main log file."""
         instance_count = 2
         log_name = LOG_PATH + 'test_log_name'
-        sim_learn.main(["sim_learn", "8", "2", "id", "xor", "10", "1", str(instance_count), "1234", "1234", log_name])
+        sim_learn.main(["8", "2", "id", "xor", "10", "1", str(instance_count), "1234", "1234", log_name])
 
         def line_count(file_object):
             """
@@ -88,7 +89,8 @@ class TestSimLearn(unittest.TestCase):
         expected_number_of_result = instance_count * restarts
         log_name = LOG_PATH + 'test_number_of_results'
         sim_learn.main(
-            ["sim_learn", "8", "2", "id", "xor", "10", str(restarts), str(instance_count), "1234", "1234", log_name]
+            ["8", "2", "id", "xor", "10", str(restarts), str(instance_count), "1234", "1234",
+             '--log_name={0}'.format(log_name)]
         )
 
         def line_count(file_object):
@@ -105,3 +107,10 @@ class TestSimLearn(unittest.TestCase):
         log_file = open(log_name + '.log', 'r')
         self.assertEqual(line_count(log_file), expected_number_of_result, 'Unexpected number of results')
         log_file.close()
+
+    def test_seed_challenges(self):
+        """This test the trainingset challenge generation to be deterministic with different seeds."""
+        log_name = 'test/test_log'
+        log_parameter = '--log_name={0}'.format(log_name)
+        seed_parameter = '--seed_challenges=0xBA11'
+        sim_learn.main(["8", "2", "id", "xor", "20", "1", "2", "1234", "1234", log_parameter, seed_parameter])
