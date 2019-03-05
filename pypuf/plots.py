86265a48e512fc009a3924ad0febb02c3fb0b1bd
Benjamin Zengin <b.zengin@yahoo.de>
2019-03-05 16:19:03 +0100

    Updates Figure 5 and PermutationIndexPlot

--- pypuf/plots.py
+++ pypuf/plots.py
@@ -11,6 +11,7 @@ class SuccessRatePlot:
     Show the ratio of experiment results with accuracy higher than a given threshold to the total number of results,
     dependent on the number of examples in the training set.
     """
+
     def __init__(self, filename, group_by, experiment_hashes=None, success_threshold=.7, group_labels=None):
         """
         Prepare a plot
@@ -106,7 +107,7 @@ class SuccessRatePlot:
                 success_rate[idx, 1] = \
                     group_N_results[
                         group_N_results['accuracy'] > self.success_threshold
-                    ].size / group_N_results.size
+                        ].size / group_N_results.size
             success_rate.sort(axis=0)
             self.plot_data[name] = success_rate
 
@@ -122,7 +123,8 @@ class SuccessRatePlot:
 
 
 class PermutationIndexPlot:
-    def __init__(self, filename, results, group_by, experiment_ids=None, group_labels=None, group_subplot_layout=None):
+    def __init__(self, filename, group_by, group_labels=None, group_subplot_layout=None,
+                 w=3.34, h=1.7):
         self.title_size = 4
         self.tick_size = 3
         self.x_label_size = 4
@@ -130,67 +132,57 @@ class PermutationIndexPlot:
 
         self.n = None
         self.k = None
-        self.experiment = None
         self.results = None
 
         self.filename = filename
-        self.results = results
-        self.experiment_ids = experiment_ids or []
+        # self.experiment_hashes = experiment_hashes
         self.group_by = group_by
         self.group_labels = {} if group_labels is None else group_labels
 
         self.figure = plt.figure()
-        self.figure.set_size_inches(w=3.34, h=1.7)
+        self.figure.set_size_inches(w=w, h=h)
         self.group_subplot_layout = group_subplot_layout
 
         self.plot_data = None
 
-    def plot(self):
-        self.figure.clear()
-        results = []
-        if self.experiment_ids:
-            for experiment_id in self.experiment_ids:
-                result = self.results[experiment_id]
-                if result and isinstance(result, ExperimentResult):
-                    results.append(result)
-        else:
-            results = [r for r in self.results.values() if r]
+    def plot(self, results):
+        # if self.experiment_hashes:
+        #    results = results.loc[results['experiment_hash'].isin(self.experiment_hashes)]
 
-        if len(results) == 0:
+        if results.empty:
             return
 
-        groups = sorted(set([str(getattr(r, self.group_by)) for r in results]))
-        print(groups)
-        assert len(set([r.experiment for r in results])) == 1
+        self.figure.clear()
+
+        grouped_results = results.groupby(self.group_by)
+        group_names = list(grouped_results.groups.keys())
 
         if self.group_subplot_layout:
             group_subplot_layout = self.group_subplot_layout
         else:
-            group_subplot_layout = {groups[i]: (len(groups), 1, i + 1) for i in range(len(groups))}
+            group_subplot_layout = {group_names[i]: (len(group_names), 1, i + 1) for i in range(len(group_names))}
 
-        print(group_subplot_layout)
-        assert all(group in group_subplot_layout for group in groups), "Subplot data must be given for all groups."
+        assert all(group in group_subplot_layout for group in group_names), "Subplot data must be given for all groups."
 
-        self.experiment = results[0].experiment
-
-        axis, legend = None, None
-        for i, group in enumerate(groups):
-            axis = self.figure.add_subplot(*group_subplot_layout[group])
+        axis, legend, i = None, None, 0
+        for name, group_results in grouped_results:
+            axis = self.figure.add_subplot(*group_subplot_layout[name])
             axis.tick_params(width=0.25, which='both', labelsize=self.tick_size, direction='in')
             for a in ['top', 'bottom', 'left', 'right']:
                 axis.spines[a].set_linewidth(0.5)
-            axis.set_title(self.group_labels[group] if group in self.group_labels else group, size=self.title_size)
-            group_results = [r for r in results if str(getattr(r, self.group_by)) == group]
-            permutation_indices = [
-                r.best_iteration + 1 for r in group_results if r.best_iteration >= 0
-            ]
+            axis.set_title(self.group_labels[name] if name in self.group_labels else name, size=self.title_size)
+            successful_runs = group_results[group_results['best_permutation_iteration'] > 0]
+            permutation_indices = successful_runs[['best_permutation_iteration',
+                                                   'total_permutation_iterations']].max(axis=1).tolist()
             if len(permutation_indices) == 0:
                 continue
             permutation_indices.sort()
-            print(permutation_indices)
+            print(name, permutation_indices)
             m = max(permutation_indices)
             amounts, _, _ = axis.hist(permutation_indices, density=True, label='Rel. Frequency', bins=range(1, m + 2),
                                       histtype='step', linestyle='--', linewidth=0.5)
+
+            top_pos = None
             xs = [1]
             ys = [0]
             s = 0
@@ -198,9 +190,18 @@ class PermutationIndexPlot:
                 s += amount
                 xs.append(idx + 2)
                 ys.append(s)
+                if not top_pos and s >= 0.8:
+                    top_pos = idx + 2
+
+            if top_pos:
+                axis.plot((top_pos, top_pos), (0, 0.8), linewidth=0.25)
+            axis.axhline(y=0.8, linewidth=0.25)
             axis.plot(xs, ys, label='Cum. Rel. Frequency', linewidth=0.5)
-            if i == len(groups) // 2:
+            axis.scatter(top_pos, 0.8, s=2, zorder=5)
+
+            if i == len(group_names) // 2:
                 axis.legend(loc=7, fontsize=self.legend_size)
+            i += 1
 
         plt.tight_layout()
         self.figure.savefig(self.filename)
--- pypuf/studies/breaking_lightweight_secure/fig_05.py
+++ pypuf/studies/breaking_lightweight_secure/fig_05.py
@@ -2,13 +2,13 @@
 Index of the first successful permutation for the correlation attack.
 """
 
-from pypuf.experiments.experiment.correlation_attack import ExperimentCorrelationAttack
+from pypuf.experiments.experiment.correlation_attack import ExperimentCorrelationAttack, Parameters
 from pypuf.plots import PermutationIndexPlot
 from pypuf.studies.base import Study
 
 
 class BreakingLightweightSecureFig05(Study):
-    SAMPLES_PER_GRAPH = 1000
+    SAMPLES_PER_GRAPH = 100
 
     KS = [4, 4, 5, 6]
     TRAINING_SET_SIZES = [12000, 30000, 300000, 1000000]
@@ -21,40 +21,48 @@ class BreakingLightweightSecureFig05(Study):
         return 'breaking_lightweight_secure_fig_05'
 
     def plot(self):
-        if not self.results:
+        if self.experimenter.results.empty:
             return
 
-        if not self.result_plot:
-            self.result_plot = PermutationIndexPlot(
-                filename='figures/breaking_lightweight_secure_fig_05.pdf',
-                results=self.results,
-                group_by='N',
-                group_labels={
-                    str(self.TRAINING_SET_SIZES[i]):
-                        "Perm. Index Dist. (k={}, {:,} CRPs)".format(self.KS[i], self.TRAINING_SET_SIZES[i])
-                    for i in range(len(self.TRAINING_SET_SIZES))
-                },
-                #group_subplot_layout={
-                #    self.TRAINING_SET_SIZES[0]: (1, 2, 1)
-                #}
-            )
-
-        self.result_plot.plot()
+        self.result_plot.plot(self.experimenter.results)
 
     def experiments(self):
         experiments = []
+
+        self.result_plot = PermutationIndexPlot(
+            filename='figures/breaking_lightweight_secure_fig_05.pdf',
+            group_by='N',
+            group_labels={
+                self.TRAINING_SET_SIZES[i]:
+                    "Perm. Index Dist. (k={}, {:,} CRPs)".format(self.KS[i], self.TRAINING_SET_SIZES[i])
+                for i in range(len(self.TRAINING_SET_SIZES))
+            },
+            group_subplot_layout={
+                self.TRAINING_SET_SIZES[0]: (4, 2, 1),
+                self.TRAINING_SET_SIZES[1]: (4, 2, 2),
+                self.TRAINING_SET_SIZES[2]: (4, 1, 2),
+                self.TRAINING_SET_SIZES[3]: (4, 1, 3),
+            }, w=3.34, h=3.9
+        )
+
         for idx, training_set_size in enumerate(self.TRAINING_SET_SIZES):
             for i in range(self.SAMPLES_PER_GRAPH):
                 experiments.append(
                     ExperimentCorrelationAttack(
                         progress_log_prefix=self.name(),
-                        n=64,
-                        k=self.KS[idx],
-                        N=training_set_size,
-                        seed_instance=314159 + i,
-                        seed_model=265358 + i,
-                        seed_challenge=979323 + i,
-                        seed_challenge_distance=846264 + i,
+                        parameters=Parameters(
+                            n=64,
+                            k=self.KS[idx],
+                            N=training_set_size,
+                            seed_instance=314159 + i,
+                            seed_model=265358 + i,
+                            seed_challenge=979323 + i,
+                            seed_distance=846264 + i,
+                            lr_iteration_limit=1000,
+                            mini_batch_size=10000,
+                            convergence_decimals=2,
+                            shuffle=True
+                        )
                     )
                 )
         return experiments
