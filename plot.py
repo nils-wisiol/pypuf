38646f0373a58252a1f8bd1ebbd70800af2a0967
Nils Wisiol <mail@nils-wisiol.de>
2019-03-22 10:45:50 +0100

    Enables plotting of results without running incomplete experiments

--- plot.py
+++ plot.py
@@ -1,5 +1,5 @@
 """
-Helper module to run studies as defined in pypuf.studies
+Helper module to plot a study result as defined in pypuf.studies
 """
 import sys
 import argparse
@@ -10,20 +10,20 @@ from pypuf.tools import find_study_class
 
 def main(args):
     """
-    Tries to find the study specified and runs it.
+    Tries to find the study specified and plots it.
     """
     parser = argparse.ArgumentParser(
         prog='study',
-        description="Runs a pypuf study",
+        description="Plot results from a pypuf study",
     )
-    parser.add_argument("study", help="name of the study to be run", type=str)
+    parser.add_argument("study", help="name of the study to be plotted", type=str)
 
     args = parser.parse_args(args)
 
     study_class = find_study_class(args.study)
-    print('Running {}.{}'.format(study_class.__module__, study_class.__name__))
+    print('Plotting results for {}.{}'.format(study_class.__module__, study_class.__name__))
     study = study_class()
-    study.run()
+    study.plot()
 
 
 if __name__ == '__main__':
