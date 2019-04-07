1610d92592ae04b0869b95c5ecc83728c58f6f8c
Nils Wisiol <mail@nils-wisiol.de>
2019-04-07 20:42:26 +0200

    increase study size, fix plots

--- pypuf/studies/ipmod2_transform/reliability.py
+++ pypuf/studies/ipmod2_transform/reliability.py
@@ -1,7 +1,6 @@
 import re
 
-from matplotlib.pyplot import subplots
-from seaborn import barplot
+from seaborn import catplot
 
 from pypuf.experiments.experiment.property_test import ExperimentPropertyTest, Parameters
 from pypuf.simulation.arbiter_based.ltfarray import CompoundTransformation, LTFArray
@@ -18,14 +17,14 @@ class ReliabilityStudy(Study):
                 progress_log_name=None,
                 parameters=Parameters(
                     test_function='reliability_statistic',
-                    challenge_count=100,
-                    measurements=20,
+                    challenge_count=1000,
+                    measurements=100,
                     challenge_seed=31415,
                     ins_gen_function='create_noisy_ltf_arrays',
                     param_ins_gen={
                         'n': n,
                         'k': k,
-                        'instance_count': 10,
+                        'instance_count': 100,
                         'transformation': CompoundTransformation(
                             generator=LTFArray.generate_ipmod2_transform,
                             args=(n, k, NoisySRAM((k, n, n), seed=31415, noise=sram_noise)),
@@ -38,8 +37,8 @@ class ReliabilityStudy(Study):
                     }
                 )
             )
-            for sram_noise in [.00001, .0001, .001, .01, .1, .2, .5]
-            for arbiter_noise in [.00001, .0001, .001, .01, .1, .2, .5]
+            for sram_noise in [0, .00001, .00005, .0001, .0005, .001, .01, .1, .2, .5]
+            for arbiter_noise in [0, .00001, .00005, .0001, .0005, .001, .01, .1, .2, .5]
             for k in [1, 2, 4, 6, 8]
         ]
 
@@ -55,18 +54,22 @@ class ReliabilityStudy(Study):
             r'\1',
             row['param_ins_gen']
         ), axis=1)
+        data['k'] = data.apply(lambda row: re.sub(
+            r'.*\'k\': ([^\,\}]+).*',
+            r'\1',
+            row['param_ins_gen']
+        ), axis=1)
 
-        fig, ax = subplots(1, 1)
-
-        barplot(
+        facet = catplot(
             x='sram_noise',
             y='mean',
+            col='k',
             hue='arbiter_noise',
+            kind='bar',
             data=data,
-            ax=ax,
         )
 
-        ax.set_xlabel('SRAM Noise Level')
-        ax.set_ylabel('Mean Reliability')
-        fig.suptitle('IP Mod 2 Transform Reliabilty')
-        fig.savefig('figures/%s.pdf' % self.name(), bbox_inches='tight', pad_inches=.5)
+        facet.set_axis_labels('SRAM Noise Level', 'Reliability')
+        facet.fig.subplots_adjust(top=.8, wspace=.02, hspace=.02);
+        facet.fig.suptitle('IP Mod 2 Transform Reliabilty')
+        facet.fig.savefig('figures/%s.pdf' % self.name(), bbox_inches='tight', pad_inches=.5)
