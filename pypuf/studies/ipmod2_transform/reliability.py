0bdce83d9aa3a3b51247cb4a00a85ea85340db9c
Nils Wisiol <mail@nils-wisiol.de>
2019-04-07 14:44:28 +0200

    Expand reliability study for ipmod2 transform, postprocess results

--- pypuf/studies/ipmod2_transform/reliability.py
+++ pypuf/studies/ipmod2_transform/reliability.py
@@ -1,3 +1,5 @@
+import re
+
 from pypuf.experiments.experiment.property_test import ExperimentPropertyTest, Parameters
 from pypuf.simulation.arbiter_based.ltfarray import CompoundTransformation, LTFArray
 from pypuf.simulation.weak import NoisySRAM
@@ -7,7 +9,7 @@ from pypuf.studies.base import Study
 class ReliabilityStudy(Study):
 
     def experiments(self):
-        (n, k) = (64, 2)
+        n = 64
         return [
             ExperimentPropertyTest(
                 progress_log_name=None,
@@ -24,6 +26,7 @@ class ReliabilityStudy(Study):
                         'transformation': CompoundTransformation(
                             generator=LTFArray.generate_ipmod2_transform,
                             args=(n, k, NoisySRAM((k, n, n), seed=31415, noise=sram_noise)),
+                            name='transform_ipmod2_NoisySRAM_31415_%f' % sram_noise
                         ),
                         'combiner': 'xor',
                         'bias': None,
@@ -33,6 +36,20 @@ class ReliabilityStudy(Study):
                 )
             )
             for sram_noise in [.00001, .0001, .001, .01, .1, .2, .5]
-            for arbiter_noise in [0]  # [.00001, .0001, .001, .01, .1, .2, .5]
-            #for k in [1, 2, 4, 6, 8]
+            for arbiter_noise in [.00001, .0001, .001, .01, .1, .2, .5]
+            for k in [1, 2, 4, 6, 8]
         ]
+
+    def plot(self):
+        data = self.experimenter.results
+        data['sram_noise'] = data.apply(lambda row: re.sub(
+            r'.*\'transformation\': transform_ipmod2_NoisySRAM_[0-9]+_([0-9\.]+).*',
+            r'\1',
+            row['param_ins_gen']
+        ), axis=1)
+        data['arbiter_noise'] = data.apply(lambda row: re.sub(
+            r'.*\'sigma_noise\': ([^\,]+).*',
+            r'\1',
+            row['param_ins_gen']
+        ), axis=1)
+        self.experimenter.save_results()
