856a14204752d9992ce48e3f624ad3f51d6db523
Nils Wisiol <mail@nils-wisiol.de>
2019-04-11 14:28:42 +0200

    work in progress

--- pypuf/simulation/weak.py
+++ pypuf/simulation/weak.py
@@ -10,7 +10,7 @@ class NoisySRAM(Simulation):
         self._seed_noise = seed_noise
         self._seed_skew = seed_skew
         self.noise = noise
-        self._next_noise_seed = seed_noise
+        self._noise_prng = RandomState(seed=seed_noise)
         self._size = size
         self._skew = RandomState(seed=seed_skew).normal(size=size)
 
@@ -28,8 +28,7 @@ class NoisySRAM(Simulation):
         skew = broadcast_to(self._skew, (N, self._size))
 
         # Different noise for every evaluation
-        noise = RandomState(seed=self._next_noise_seed).normal(scale=self.noise, size=(N, self._size))
-        self._next_noise_seed += 1
+        noise = self._noise_prng.normal(scale=self.noise, size=(N, self._size))
 
         return sign(skew + noise)
 
