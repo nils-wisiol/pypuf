d8665e949c7f4e50609cf08b3ccc34262ff17d14
Chrism7 <chrism@mi.fu-berlin.de>
2019-11-08 14:15:00 +0100

    Re-adds experiment class to MLP study

--- pypuf/studies/mlp/xor_arbiter_puf.py
+++ pypuf/studies/mlp/xor_arbiter_puf.py
@@ -6,10 +6,155 @@ The Deep Learning technique used here is the Optimization technique Adam for a S
 Forward Neural Network architecture called Multilayer Perceptron (MLP). Implementations of the MLP and Adam are
 used from Scikit-Learn and Tensorflow, respectively.
 """
+from os import getpid
+from typing import NamedTuple, Iterable
+from uuid import UUID, uuid4
+
+from numpy.random.mtrand import RandomState
 from seaborn import catplot
 
+from pypuf import tools
+from pypuf.experiments.experiment.base import Experiment
+from pypuf.learner.neural_networks.mlp_skl import MultiLayerPerceptronScikitLearn
+from pypuf.simulation.arbiter_based.arbiter_puf import XORArbiterPUF
 from pypuf.studies.base import Study
-from pypuf.studies.mlp.aseeri import Parameters, ExperimentMLPScikitLearn
+
+
+class Parameters(NamedTuple):
+    """
+    Define all input parameters for the Experiment.
+    """
+    seed_simulation: int
+    seed_challenges: int
+    seed_model: int
+    seed_distance: int
+    n: int
+    k: int
+    N: int
+    validation_frac: float
+    transformation: str
+    combiner: str
+    preprocessing: str
+    layers: Iterable[int]
+    activation: str
+    loss: str
+    domain_in: int
+    domain_out: int
+    learning_rate: float
+    penalty: float
+    beta_1: float
+    beta_2: float
+    tolerance: float
+    patience: int
+    iteration_limit: int
+    batch_size: int
+    print_learning: bool
+
+
+class Result(NamedTuple):
+    """
+    Define all parameters to be documented within the result file that are not included in the input parameters.
+    """
+    name: str
+    experiment_id: UUID
+    pid: int
+    measured_time: float
+    iterations: int
+    accuracy: float
+    loss_curve: Iterable[float]
+    accuracy_curve: Iterable[float]
+    max_memory: int
+
+
+class ExperimentMLPScikitLearn(Experiment):
+    """
+    This Experiment uses the Scikit-learn implementation of the Multilayer Perceptron Learner.
+    """
+
+    NAME = 'Multilayer Perceptron (scikit-learn)'
+
+    def __init__(self, progress_log_prefix, parameters):
+        self.id = uuid4()
+        progress_log_name = None if not progress_log_prefix else \
+            '{}_{}'.format(progress_log_prefix, self.id)
+        super().__init__(progress_log_name=progress_log_name, parameters=parameters)
+        self.training_set = None
+        self.simulation = None
+        self.learner = None
+        self.model = None
+        assert self.parameters.domain_out == -1, 'domain_out other than -1 is not supported, sorry.'
+        assert self.parameters.loss == 'log_loss', 'loss other than log_loss is not supported, sorry.'
+
+    def prepare(self):
+        """
+        Prepare learning: initialize learner, prepare training set, etc.
+        """
+        self.progress_logger.debug('Setting up simulation')
+        self.simulation = XORArbiterPUF(
+            n=self.parameters.n,
+            k=self.parameters.k,
+            seed=self.parameters.seed_simulation,
+            transform=self.parameters.transformation,
+        )
+        self.progress_logger.debug(f'Gathering training set with {self.parameters.N} examples')
+        self.training_set = tools.TrainingSet(
+            instance=self.simulation,
+            N=self.parameters.N,
+            random_instance=RandomState(seed=self.parameters.seed_challenges),
+        )
+        self.progress_logger.debug('Setting up learner')
+        self.learner = MultiLayerPerceptronScikitLearn(
+            n=self.parameters.n,
+            k=self.parameters.k,
+            training_set=self.training_set,
+            validation_frac=self.parameters.validation_frac,
+            transformation=self.simulation.transform,
+            preprocessing=self.parameters.preprocessing,
+            layers=self.parameters.layers,
+            learning_rate=self.parameters.learning_rate,
+            penalty=self.parameters.penalty,
+            beta_1=self.parameters.beta_1,
+            beta_2=self.parameters.beta_2,
+            tolerance=self.parameters.tolerance,
+            patience=self.parameters.patience,
+            iteration_limit=self.parameters.iteration_limit,
+            batch_size=self.parameters.batch_size,
+            seed_model=self.parameters.seed_model,
+            print_learning=self.parameters.print_learning,
+            logger=self.progress_logger.debug,
+        )
+        self.learner.prepare()
+
+    def run(self):
+        """
+        Execute the learning process.
+        """
+        self.progress_logger.debug('Starting learner')
+        self.model = self.learner.learn()
+
+    def analyze(self):
+        """
+        Calculate statistics of the trained model and write them into a result log file.
+        """
+        self.progress_logger.debug('Analyzing result')
+        assert self.model is not None
+        accuracy = 1.0 - tools.approx_dist(
+            instance1=self.simulation,
+            instance2=self.model,
+            num=10**4,
+            random_instance=RandomState(seed=self.parameters.seed_distance),
+        )
+        return Result(
+            name=self.NAME,
+            experiment_id=self.id,
+            pid=getpid(),
+            measured_time=self.measured_time,
+            iterations=self.learner.nn.n_iter_,
+            accuracy=accuracy,
+            loss_curve=[round(loss, 3) for loss in self.learner.nn.loss_curve_],
+            accuracy_curve=[round(accuracy, 3) for accuracy in self.learner.accuracy_curve],
+            max_memory=self.max_memory(),
+        )
 
 
 class MLPDiversePUFsStudy(Study):
