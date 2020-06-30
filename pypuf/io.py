d3e2c4b3199db3ca900e1e23e67cd7fd90cf6197
Nils Wisiol <mail@nils-wisiol.de>
2020-06-30 22:58:25 +0200

    io: adds challenge reliability sets

--- pypuf/io.py
+++ pypuf/io.py
@@ -5,6 +5,7 @@ from numpy import abs as np_abs, ndarray, empty
 from numpy import copy, int8
 from numpy import sum as np_sum
 from numpy.random import RandomState
+import numpy as np
 
 from .simulation.base import Simulation
 
@@ -40,50 +41,70 @@ def approx_stabilities(instance: Simulation, num: int, reps: int, seed: int) -> 
     return 0.5 + 0.5 * np_abs(np_sum(responses, axis=0)) / reps
 
 
-class ChallengeResponseSet:
-    # TODO remove when removing python 3.7 support
-    #  (replace with from __future__ import annotations)
-    pass
+class ChallengeInformationSet:
+    # TODO fix type annotation when removing python 3.7 support
 
-
-class ChallengeResponseSet:
-
-    def __init__(self, challenges: ndarray, responses: ndarray) -> None:
-        if challenges.shape[0] != responses.shape[0]:
-            raise ValueError('Must supply an equal number of challenges and responses.')
+    def __init__(self, challenges: ndarray, information: ndarray) -> None:
+        if challenges.shape[0] != information.shape[0]:
+            raise ValueError('Must supply an equal number of challenges and information about these challenges.')
         self.challenges = challenges
-        self.responses = responses
+        self.information = information
         self.N = len(self.challenges)
 
     @property
     def challenge_length(self) -> int:
         return self.challenges.shape[1]
 
-    @property
-    def response_length(self) -> int:
-        return self.responses.shape[1]
-
     def __len__(self) -> int:
         return self.challenges.shape[0]
 
-    def __getitem__(self, item: Union[slice, int]) -> ChallengeResponseSet:
-        return ChallengeResponseSet(self.challenges[item], self.responses[item])
+    def __getitem__(self, item: Union[slice, int]) -> object:
+        return self.__class__(self.challenges[item], self.information[item])
 
-    def random_subset(self, N: Union[int, float]) -> ChallengeResponseSet:
+    def random_subset(self, N: Union[int, float]) -> object:
         if N < 1:
             N = int(self.N * N)
         return self[sample(range(self.N), N)]
 
-    def block_subset(self, i: int, total: int) -> ChallengeResponseSet:
+    def block_subset(self, i: int, total: int) -> object:
         return self[int(i / total * self.N):int((i + 1) / total * self.N)]
 
 
-class SimulationChallengeResponseSet(ChallengeResponseSet):
+class ChallengeResponseSet(ChallengeInformationSet):
 
-    def __init__(self, instance: Simulation, N: int, seed: int, r: int = 1) -> None:
-        self.instance = instance
+    @classmethod
+    def from_simulation(cls, instance: Simulation, N: int, seed: int, r: int = 1) -> ChallengeInformationSet:
         challenges = random_inputs(instance.challenge_length, N, seed)
-        super().__init__(
+        crp_set = cls(
             challenges=challenges,
             responses=instance.r_eval(r, challenges)
         )
+        crp_set.instance = instance
+        return crp_set
+
+    def __init__(self, challenges: ndarray, responses: ndarray) -> None:
+        super().__init__(challenges, responses)
+        self.responses = responses
+
+    @property
+    def response_length(self) -> int:
+        return self.responses.shape[1]
+
+
+class ChallengeReliabilitySet(ChallengeInformationSet):
+
+    @classmethod
+    def from_simulation(cls, instance: Simulation, N: int, seed: int, r: int = 5) -> ChallengeInformationSet:
+        # noinspection PyTypeChecker
+        return cls.from_challenge_response_set(ChallengeResponseSet.from_simulation(instance, N, seed, r))
+
+    @classmethod
+    def from_challenge_response_set(cls, crp_set: ChallengeResponseSet) -> ChallengeInformationSet:
+        return cls(
+            challenges=crp_set.challenges,
+            reliabilities=np.average(crp_set.responses, axis=-1),
+        )
+
+    def __init__(self, challenges: ndarray, reliabilities: ndarray) -> None:
+        super().__init__(challenges, reliabilities)
+        self.reliabilities = reliabilities
