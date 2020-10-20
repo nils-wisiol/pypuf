41aa2025d003789ffb76e55ec0a7f0fa7d3c294a
Nils Wisiol <mail@nils-wisiol.de>
2020-10-20 13:48:18 +0200

    Release Version v0.0.8

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ author = 'Nils Wisiol'
 master_doc = 'index'
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.7'
+release = '0.0.8'
 
 
 # -- General configuration ---------------------------------------------------
