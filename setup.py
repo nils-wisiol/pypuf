a36aaaf888e4a0930fbe8dc91419c3c0ef7a0530
Nils Wisiol <mail@nils-wisiol.de>
2021-03-18 17:04:24 +0100

    Release Version v2.0.1

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import setuptools
 
 setup(
     name='pypuf',
-    version='2.0.0',
+    version='2.0.1',
     packages=setuptools.find_packages(),
     long_description=open('README.md').read(),
     long_description_content_type="text/markdown",
