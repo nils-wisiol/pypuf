c8982cb8fa73a6ffda6b8649bcabc4f6f9f344e6
Nils Wisiol <mail@nils-wisiol.de>
2021-06-16 17:54:41 +0200

    Release Version v2.3.1

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ author = 'Nils Wisiol'
 master_doc = 'index'
 
 # The full version, including alpha/beta/rc tags
-release = '2.3.0'
+release = '2.3.1'
 
 
 # -- General configuration ---------------------------------------------------
