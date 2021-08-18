cf49c8e85a851920b2b954f4706860468114faac
Nils Wisiol <mail@nils-wisiol.de>
2021-08-18 12:06:19 +0200

    io: removes unused code

--- pypuf/io.py
+++ pypuf/io.py
@@ -1,18 +1,14 @@
 from random import sample
 from typing import Union, Tuple
 
-from numpy import abs as np_abs, ndarray, empty
-from numpy import copy, int8
-from numpy import sum as np_sum
-from numpy.random import RandomState
 import numpy as np
+from numpy import int8
+from numpy import ndarray
+from numpy.random import RandomState
 
 from .simulation import Simulation
 
 BIT_TYPE = int8
-
-
-# TODO add documentation
 
 
 def random_inputs(n: int, N: int, seed: int) -> ndarray:
@@ -39,28 +35,6 @@ def random_inputs(n: int, N: int, seed: int) -> ndarray:
 
     """
     return 2 * RandomState(seed).randint(0, 2, (N, n), dtype=BIT_TYPE) - 1
-
-
-def transform_challenge_01_to_11(challenge: ndarray) -> ndarray:  # TODO remove
-    res = copy(challenge)
-    res[res == 1] = -1
-    res[res == 0] = 1
-    return res
-
-
-def transform_challenge_11_to_01(challenge: ndarray) -> ndarray:  # TODO remove
-    res = copy(challenge)
-    res[res == 1] = 0
-    res[res == -1] = 1
-    return res
-
-
-def approx_stabilities(instance: Simulation, num: int, reps: int, seed: int) -> ndarray:
-    challenges = random_inputs(instance.challenge_length, num, seed)
-    responses = empty((reps, num), dtype=BIT_TYPE)
-    for i in range(reps):
-        responses[i, :] = instance.eval(challenges)
-    return 0.5 + 0.5 * np_abs(np_sum(responses, axis=0)) / reps
 
 
 class ChallengeInformationSet:
