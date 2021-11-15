7d32efadc3d53a6c154dd625ddbfb53445992997
Nils Wisiol <mail@nils-wisiol.de>
2021-11-15 13:37:50 +0100

    Release Version v3.2.1

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ author = 'Nils Wisiol'
 master_doc = 'index'
 
 # The full version, including alpha/beta/rc tags
-release = '3.2.0'
+release = '3.2.1'
 
 
 # -- General configuration ---------------------------------------------------
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import setuptools
 
 setup(
     name='pypuf',
-    version='3.2.0',
+    version='3.2.1',
     packages=setuptools.find_packages(),
     long_description=open('README.md').read(),
     long_description_content_type="text/markdown",
